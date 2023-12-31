c2dcfe06f3ef8f48523290665f4e1494d9303556
pinomaker-hoo <inhoo987654321@gmail.com>
2023-12-31 18:21:09 +0900

    fix : next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
-  output: "standalone",
+  output: "export",
+  distDir: "dist",
   async rewrites() {
     return [
       {

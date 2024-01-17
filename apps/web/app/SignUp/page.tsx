2a407f684f03f24ec896099b86fb4ccc87f20cff
최혜림 <chhari0708@naver.com>
2024-01-17 22:56:40 +0900

    SignUp 퍼블리싱

--- apps/web/app/SignUp/page.tsx
+++ apps/web/app/SignUp/page.tsx
@@ -7,109 +7,79 @@ export default function Signup(): JSX.Element {
 
   return (
     <div className="flex w-full h-screen items-center justify-center bg-[#FAFAFB] ">
-      <div className="bg-white w-[900px] h-[613px] rounded-2xl flex shadow-md">
-        <div className="w-1/2 h-full  bg-[#F6F8FF] rounded-tl-2xl rounded-bl-2xl flex">
-          <div className="w-[89px] h-[27px] ml-[34px] mt-[45px] flex items-center">
-            <Image src="/dice2.png" alt="leftTop" width={19} height={17} />
-            <div className="text-[#623AD6] font-open font-medium text-xl ml-1">
-              Register
-            </div>
-          </div>
-          <div className="flex items-end justify-end w-[270px] h-[240px] mt-[332px] ml-[21px]">
-            <Image src="/dice.png" alt="My Image" width={270} height={240} />
-          </div>
+      <div className="bg-white w-[660px] h-[565px] rounded-2xl shadow-md">
+        <div className="flex justify-center font-spoqa text-[40px] font-bold mt-20">
+          Signup
         </div>
-        <div className="flex w-1/2 h-full justify-center items-center">
-          <div className="w-[330px] h-[409px]">
-            <div className="flex justify-center">
-              <div className="w-full">
-                <label
-                  htmlFor="first_name"
-                  className="block mb-[14px] text-base font-medium text-gray-900 dark:text-black font-spoqa"
-                >
-                  Email
-                </label>
-                <input
-                  type="text"
-                  id="first_name"
-                  className="font-normal font-spoqa border h-[50px] text-gray-900 text-base p-4 rounded-lg block w-full border-[#EBEBEC] placeholder-[#DDD] dark:text-black "
-                  placeholder="Enter Your Email"
-                  required
-                />
-              </div>
-            </div>
-            <div className="flex justify-center">
-              <div className="w-full mt-5">
-                <label
-                  htmlFor="first_name"
-                  className="font-spoqa block mb-[14px] text-base font-medium text-gray-900 dark:text-black"
-                >
-                  Password
-                </label>
-                <input
-                  type="text"
-                  id="first_name"
-                  className="font-normal font-spoqa border h-[50px] text-gray-900 text-base p-4 rounded-lg block w-full border-[#EBEBEC] placeholder-[#DDD] dark:text-black "
-                  placeholder="Enter Your Password"
-                  required
-                />
-              </div>
-            </div>
-            <div className="flex justify-center">
-              <div className="w-full mt-12">
-                <button
-                  className="w-full bg-[#623AD6] text-white rounded-xl h-[55px] text-lg font-bold font-spoqa"
-                  onClick={handleSignup}
-                >
-                  LOGIN
-                </button>
-              </div>
-            </div>
-            <div className="flex justify-end text-sm underline w-full text-[#676767] mt-4 mr-12 font-spoqa font-medium">
-              HIDICE Signup
-            </div>
-            <div className="w-full flex justify-between m-auto mt-[46px]">
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full shadow-md">
-                <Image
-                  src="/Microsoft.png"
-                  alt="Microsoft"
-                  width={24}
-                  height={24}
-                />
-              </div>
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full shadow-md">
-                <Image
-                  src="/google.png"
-                  alt="Microsoft"
-                  width={24}
-                  height={24}
-                />
-              </div>
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full shadow-md">
-                <Image
-                  src="/apple.png"
-                  alt="Microsoft"
-                  width={28}
-                  height={28}
-                />
-              </div>
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full shadow-md">
-                <Image
-                  src="/github.png"
-                  alt="Microsoft"
-                  width={28}
-                  height={28}
-                />
-              </div>
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full shadow-md">
-                <Image
-                  src="/twitter.png"
-                  alt="Microsoft"
-                  width={24}
-                  height={19.83}
-                />
-              </div>
-            </div>
+        <div>
+          <div className="flex mt-[60px] w-[540px] h-[50px] items-center m-auto">
+            <label
+              htmlFor="first_name"
+              className="text-base font-medium text-black font-spoqa mr-[65px]"
+            >
+              Email
+              <span className="ml-1 text-base font-medium text-[#F45050] font-spoqa">
+                *
+              </span>
+            </label>
+            <input
+              type="text"
+              id="Email"
+              className="font-normal font-spoqa border h-[50px] w-[420px] text-gray-900 text-base p-4 rounded-lg block border-[#EBEBEC] placeholder-[#DDD] dark:text-black "
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
+          <div className="flex mt-5 w-[540px] h-[50px] items-center m-auto">
+            <label
+              htmlFor="password"
+              className="text-base font-medium text-black font-spoqa mr-[37px]"
+            >
+              Password
+              <span className="ml-1 text-base font-medium text-[#F45050] font-spoqa">
+                *
+              </span>
+            </label>
+            <input
+              type="text"
+              id="Email"
+              className="font-normal font-spoqa border h-[50px] w-[200px] mr-5 text-gray-900 text-base p-4 rounded-lg block border-[#EBEBEC] placeholder-[#DDD] dark:text-black "
+              placeholder="Enter Your Password"
+              required
+            />
+            <input
+              type="text"
+              id="Email"
+              className="font-normal font-spoqa border h-[50px] w-[200px] text-gray-900 text-base p-4 rounded-lg block border-[#EBEBEC] placeholder-[#DDD] dark:text-black "
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+          <div className="flex mt-5 w-[540px] h-[50px] items-center m-auto">
+            <label
+              htmlFor="NickName"
+              className="text-base font-medium text-black font-spoqa mr-[32px]"
+            >
+              NickName
+              <span className="ml-1 text-base font-medium text-[#F45050] font-spoqa">
+                *
+              </span>
+            </label>
+            <input
+              type="text"
+              id="Email"
+              className="font-normal font-spoqa border h-[50px] w-[420px] text-gray-900 text-base p-4 rounded-lg block border-[#EBEBEC] placeholder-[#DDD] dark:text-black "
+              placeholder="Enter Your NickName"
+              required
+            />
+          </div>
+          <div className="w-[540px] h-[55px] flex m-auto mt-[50px] justify-between">
+            <button className="h-full w-[257px] bg-[#EBEBEC] rounded-[15px] text-white font-spoqa font-bold">
+              CANCEL
+            </button>
+            <button className="h-full w-[257px] bg-[#623AD6] rounded-[15px] text-white font-spoqa font-bold">
+              JOIN
+            </button>
           </div>
         </div>
       </div>

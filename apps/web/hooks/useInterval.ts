cbd796d3563ee434cf01b4b2d0839cc66930b9a4
pinomaker-hoo <inhoo987654321@gmail.com>
2024-01-22 21:37:26 +0900

    feat : Add Use Interval

---
+++ apps/web/hooks/useInterval.ts
@@ -0,0 +1,20 @@
+// useInterval.js
+import { useEffect, useRef } from 'react'
+
+const useInterval = (callback: () => void, delay: number) => {
+  const savedCallback = useRef(callback)
+
+  useEffect(() => {
+    savedCallback.current = callback
+  })
+
+  useEffect(() => {
+    if (delay === null) return
+
+    const timer = setInterval(() => savedCallback.current(), delay)
+
+    return () => clearInterval(timer)
+  }, [delay])
+}
+
+export default useInterval

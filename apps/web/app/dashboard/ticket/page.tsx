e4a795f1750837e1087a71e9b79979b83b4c3db9
pinomaker-hoo <inhoo987654321@gmail.com>
2024-01-25 02:12:53 +0900

    Create page.tsx

---
+++ apps/web/app/dashboard/ticket/page.tsx
@@ -0,0 +1,12 @@
+const TicketPage = () => {
+  return (
+    <div className="p-5">
+      <div>
+        <h1 className="text-3xl font-bold">Ticket / Epic</h1>
+      </div>
+      <div className="mt-6 h-[180px] w-full bg-white rounded-3xl shadow-md"></div>
+    </div>
+  );
+};
+
+export default TicketPage;

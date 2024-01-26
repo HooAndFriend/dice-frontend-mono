a0453f1a1fadd5dd24daeff467be96c4f3780258
최혜림 <chhari0708@naver.com>
2024-01-26 22:54:35 +0900

    creare workspace
    container 구조로 변경

--- apps/web/app/create-workspace/page.tsx
+++ apps/web/app/create-workspace/page.tsx
@@ -1,31 +1,16 @@
-export default function createWorkspace() {
+// ** Component Imports
+import CreateWorkspaceContainer from '@/container/createworkspace-container';
+
+// ** Provider Imports
+import RocoilRootProvider from '@/components/provider/recoil-provider';
+import {DialogProvider} from '@/context/DialogContext';
+
+export default function Signup(): JSX.Element {
   return (
-    <div className="w-full h-screen bg-[#FAFAFB] flex items-center justify-center font-spoqa">
-      <div className="w-[330px] h-[513px]">
-        <img
-          className="m-auto"
-          src="images/workspaceLogo.png"
-          width={192}
-          height={192}
-        />
-        <div className="mt-[40px]">
-          <label className="font-medium">name</label>
-          <input
-            className="w-full h-[50px] rounded-[10px] border border-[#EBEBEC] placeholder-[#DDDDDD] p-4 mt-[14px]"
-            placeholder="Enter Your Email"
-          />
-        </div>
-        <div className="mt-5">
-          <label className="font-medium">description</label>
-          <input
-            className="w-full h-[50px] rounded-[10px] border border-[#EBEBEC] placeholder-[#DDDDDD] p-4 mt-[14px]"
-            placeholder="Enter Your description"
-          />
-        </div>
-        <button className="w-full h-[55px] bg-main rounded-[15px] text-white text-lg font-bold mt-[30px]">
-          CREATE
-        </button>
-      </div>
-    </div>
+    <RocoilRootProvider>
+      <DialogProvider>
+        <CreateWorkspaceContainer />
+      </DialogProvider>
+    </RocoilRootProvider>
   );
 }

280240864e370ff3835ba36306fb707ebe9cde75
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-08 19:25:26 +0900

    feat : Team Setting

--- apps/web/src/type/team/index.d.ts
+++ apps/web/src/type/team/index.d.ts
@@ -5,8 +5,18 @@ export interface GetUserTeamListResponse extends CommonResponse {
   data: TeamUserInfo[];
 }
 
+export interface GetTeamResponse extends CommonResponse, TeamInfo {}
+
 export interface TeamUserInfo {
   id: number;
   role: RoleType;
   team: { name: string; profile: string; id: number; uuid: string };
+}
+
+export interface TeamInfo {
+  id: number;
+  name: string;
+  profile: string;
+  uuid: string;
+  description: string;
 }

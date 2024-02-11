349e4509427a0c79815676831cfd53d683ea9d39
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-12 00:15:47 +0900

    fix : Update Team Role

--- apps/web/src/app/team/index.ts
+++ apps/web/src/app/team/index.ts
@@ -1,18 +1,21 @@
 import { atom } from "recoil";
 import { persistAtom } from "../util";
+import { RoleType } from "@/src/type/common";
 
 interface TeamStateType {
   id: number;
   name: string;
   profile: string;
   uuid: string;
+  role: RoleType;
 }
 
-export const teamInitState = {
+export const teamInitState: TeamStateType = {
   id: 0,
   name: "",
   profile: "",
   uuid: "",
+  role: "",
 };
 
 export const TeamState = atom<TeamStateType>({

fc21f3120ac9ce65f284095950ebd07e2771ad1f
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-13 17:55:20 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,15 +2,19 @@ if [ $# -eq 0 ]; then
     echo "인자가 없습니다. 스크립트를 호출할 때 인자를 전달해주세요."
     exit 1
 elif [ $1 = "web" ]; then
+    ssh -t dice "rm -rf html/www"
     scp -r ./html/www/* dice:~/html/www/
     ssh -t dice "sh restart.sh nginx"
 elif [ $1 = "admin" ]; then
+    ssh -t dice "rm -rf html/admin"
     scp -r ./html/admin/* dice:~/html/admin/
     ssh -t dice "sh restart.sh nginx"
 elif [ $1 = "download" ]; then
+    ssh -t dice "rm -rf html/download"
     scp -r ./html/download/* dice:~/html/download/
     ssh -t dice "sh restart.sh nginx"
 elif [ $1 = "all" ]; then
+    ssh -t dice "rm -rf html/*"
     scp -r ./html/* dice:~/html/
     ssh -t dice "sh restart.sh nginx"
 else

64126389169c453736b703b88fb24cc7da3b4869
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-15 00:20:33 +0900

    feat : Admin And Auth Recoil State

---
+++ apps/admin/src/app/auth/index.ts
@@ -0,0 +1,18 @@
+import { atom } from 'recoil'
+import { persistAtom } from '../util'
+
+interface AuthStateProps {
+  accessToken: string
+  refreshToken: string
+}
+
+export const AuthDefaultState: AuthStateProps = {
+  accessToken: '',
+  refreshToken: '',
+}
+
+export const AuthState = atom<AuthStateProps>({
+  key: 'authState',
+  default: AuthDefaultState,
+  effects_UNSTABLE: [persistAtom],
+})

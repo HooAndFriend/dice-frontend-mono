47567607021491e23a9440d0cef565bcc5150ead
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-14 23:25:49 +0900

    del : remove mui config

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -2,31 +2,23 @@
 import { Routes, Route } from 'react-router-dom'
 
 // ** Router Imports
-import TestPage from './pages/test-age'
+import LoginPage from './pages/login-page'
 
 // ** Context Imports
 import { ErrorProvider } from './context/ErrorContext'
 import { DialogProvider } from './context/DialogContext'
 
 // ** Style Imports
-import { ThemeProvider } from '@mui/material'
-import { theme } from '@/src/theme'
-import { GlobalStyles } from '@mui/material'
-import CssBaseline from '@mui/material/CssBaseline'
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles styles={{ backgroundColor: 'blue' }} />
-      <CssBaseline />
-      <ErrorProvider>
-        <DialogProvider>
-          <Routes>
-            <Route path="/" element={<TestPage />} />
-          </Routes>
-        </DialogProvider>
-      </ErrorProvider>
-    </ThemeProvider>
+    <ErrorProvider>
+      <DialogProvider>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </DialogProvider>
+    </ErrorProvider>
   )
 }
 

5ea839700dd8a3bede8f9028dd6dea383270a32e
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-20 00:13:02 +0900

    Update layout.tsx

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,12 +1,29 @@
+"use client";
+
+// ** Next Imports
+import { useRouter } from "next/navigation";
+
 // ** Component Imports
 import DashboardSidebard from "@/src/components/dashboard/dashboard-sidebar";
 import DashboardHeader from "@/src/components/dashboard/dashboard-header";
+
+// ** Recoil Imports
+import { useRecoilValue } from "recoil";
+import { AuthState } from "@/src/app";
 
 const DashboardRayout = ({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element => {
+  const { accessToken } = useRecoilValue(AuthState);
+
+  const router = useRouter();
+
+  if (accessToken === "") {
+    router.push("/");
+  }
+
   return (
     <div className="flex flex-col h-screen">
       <DashboardHeader />

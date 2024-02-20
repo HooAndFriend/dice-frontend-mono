a53884d89575ed3b7afbcc71ac67679e376057b6
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-21 00:41:12 +0900

    feat : Add Ticket SideBar

--- apps/web/src/components/dashboard/ticket-sidebar/index.tsx
+++ apps/web/src/components/dashboard/ticket-sidebar/index.tsx
@@ -1,8 +1,53 @@
+"use client";
+// ** Next Imports
+import { usePathname } from "next/navigation";
+
+// ** React Imports
+import { useMemo } from "react";
+
 // ** Component Imports
 import TicketSidebarView from "./ticket-sidebar";
+import TicketIcon from "@/public/svg/ticket-icon.svg";
+import EpicIcon from "@/public/svg/epic-icon.svg";
+import SprintIcon from "@/public/svg/sprint-icon.svg";
 
 const TicketSidebar = () => {
-  return <TicketSidebarView />;
+  const pathname = usePathname();
+
+  const sidebarMenuList = useMemo(
+    () =>
+      [
+        {
+          id: 1,
+          name: "Epic",
+          link: "/dashboard/ticket",
+          icon: EpicIcon,
+          isClicked: false,
+        },
+        {
+          id: 2,
+          name: "Ticket",
+          link: "/dashboard/ticket/ticket",
+          icon: TicketIcon,
+          isClicked: false,
+        },
+        {
+          id: 3,
+          name: "Sprint",
+          link: "/dashboard/ticket/sprint",
+          icon: SprintIcon,
+          isClicked: false,
+        },
+      ].map((item) => {
+        if (item.link === pathname) {
+          return { ...item, isClicked: true };
+        }
+        return item;
+      }),
+    []
+  );
+
+  return <TicketSidebarView sidebarMenuList={sidebarMenuList} />;
 };
 
 export default TicketSidebar;
--- apps/web/src/components/dashboard/ticket-sidebar/ticket-sidebar.tsx
+++ apps/web/src/components/dashboard/ticket-sidebar/ticket-sidebar.tsx
@@ -1,8 +1,19 @@
-const TicketSidebarView = () => {
+import { MenuType } from "@/src/constants/menu";
+import TicketMenuItem from "../ticket-menu-item";
+
+interface PropsType {
+  sidebarMenuList: MenuType[];
+}
+
+const TicketSidebarView = ({ sidebarMenuList }: PropsType) => {
   return (
     <div className="w-[235px] bg-white border-r-2 border-[#EBEBEC]">
       <div className="flex justify-center h-4/5">
-        <h1>Ticket</h1>
+        <div className="w-full">
+          {sidebarMenuList.map((item) => (
+            <TicketMenuItem {...item} key={item.id} />
+          ))}
+        </div>
       </div>
     </div>
   );

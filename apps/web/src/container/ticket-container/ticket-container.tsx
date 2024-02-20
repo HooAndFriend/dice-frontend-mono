75a6e4cdda966395ea7fa378592298b4684b192d
pinomaker-hoo <inhoo987654321@gmail.com>
2024-02-20 23:33:11 +0900

    feat : Ticket Filter

--- apps/web/src/container/ticket-container/ticket-container.tsx
+++ apps/web/src/container/ticket-container/ticket-container.tsx
@@ -1,3 +1,7 @@
+// ** Component Imports
+import CustomInput from "@/src/components/input/custom-input";
+import CustomSelect from "@/src/components/input/custom-select";
+
 interface PropsType {}
 
 const TicketContainerView = ({}: PropsType) => {
@@ -6,7 +10,28 @@ const TicketContainerView = ({}: PropsType) => {
       <div>
         <h1 className="text-3xl font-bold">Ticket / Epic</h1>
       </div>
-      <div className="mt-6 h-[180px] w-full bg-white rounded-3xl shadow-md"></div>
+      <div className="mt-6 h-[180px] w-full bg-white rounded-[20px] shadow-md p-4">
+        <div className="flex items-center w-full">
+          <div className="font-spoqa text-base font-bold ml-[25px] mr-[33px]">
+            Epic
+          </div>
+          <CustomSelect option="Epic" />
+          <div className="font-spoqa text-base font-bold ml-[25px] mr-[33px]">
+            Type
+          </div>
+          <CustomSelect option="Type" />
+          <div className="font-spoqa text-base font-bold ml-[25px] mr-[33px]">
+            Status
+          </div>
+          <CustomSelect option="Status" />
+        </div>
+        <div className="flex items-center w-full mt-8">
+          <div className="font-spoqa text-base font-bold ml-[25px] mr-[33px]">
+            Search
+          </div>
+          <CustomInput />
+        </div>
+      </div>
     </div>
   );
 };

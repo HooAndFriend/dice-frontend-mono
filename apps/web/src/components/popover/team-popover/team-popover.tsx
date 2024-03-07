4583731bda54bb9954ab298471aba6448196633b
rubber <rubber1926@aiaracorp.com>
2024-03-07 22:40:19 +0900

    feat(web): popover outside close event

--- apps/web/src/components/popover/team-popover/team-popover.tsx
+++ apps/web/src/components/popover/team-popover/team-popover.tsx
@@ -42,86 +42,91 @@ const TeamPopoverView = ({
   teamName,
 }: PropsType) => {
   return (
-    <div>
-      <div onClick={handleOpen}>
-        <ProfileBox image="/images/profile.jpg" alt="profile" />
-      </div>
-      {open && (
-        <div className="popover p-5 rounded-[20px] w-[350px] h-[250px] absolute bg-white shadow-md p- translate-y-10 -translate-x-3/4">
-          <div className="flex items-center justify-between">
-            <h3 className="text-lg font-bold">
-              {id === 0 ? user.nickname : teamName}
-            </h3>
-            <div className="flex items-center">
-              <img
-                src="/images/settings.png"
-                width="18px"
-                height="18px"
-                className="mr-1"
-              />
-              <h4 className="mr-3 text-sm" onClick={handleModalOpen}>
-                Setting
-              </h4>
-              <div className="flex" onClick={handleLogout}>
-                <img
-                  src="/images/logout.png"
-                  width="18px"
-                  height="18px"
-                  className="mr-1"
+    <>
+      <div>
+        <div onClick={handleOpen}>
+          <ProfileBox image="/images/profile.jpg" alt="profile" />
+        </div>
+        {open && (
+          <>
+            <div onClick={handleOpen} className="fixed inset-0 z-10" />
+            <div className="popover p-5 rounded-[20px] w-[350px] h-[250px] absolute bg-white shadow-md p- translate-y-10 -translate-x-3/4 z-10">
+              <div className="flex items-center justify-between">
+                <h3 className="text-lg font-bold">
+                  {id === 0 ? user.nickname : teamName}
+                </h3>
+                <div className="flex items-center">
+                  <img
+                    src="/images/settings.png"
+                    width="18px"
+                    height="18px"
+                    className="mr-1"
+                  />
+                  <h4 className="mr-3 text-sm" onClick={handleModalOpen}>
+                    Setting
+                  </h4>
+                  <div className="flex" onClick={handleLogout}>
+                    <img
+                      src="/images/logout.png"
+                      width="18px"
+                      height="18px"
+                      className="mr-1"
+                    />
+                    <h3 className="mr-1 text-sm">Logout</h3>
+                  </div>
+                </div>
+              </div>
+              <hr className="mt-3" />
+              <div className="mt-2 w-[350px] h-[150px] overflow-y-scroll">
+                <TeamBox
+                  id={id}
+                  teamId={0}
+                  profile={user.profile}
+                  name={user.nickname}
+                  onClick={() => handleUpdateTeam(0)}
                 />
-                <h3 className="mr-1 text-sm">Logout</h3>
+                {data.map((item) => (
+                  <TeamBox
+                    key={item.id}
+                    id={id}
+                    teamId={item.team.id}
+                    profile={item.team.profile}
+                    name={item.team.name}
+                    onClick={() => handleUpdateTeam(item)}
+                  />
+                ))}
+                <Link href="/save-team">
+                  <div className="flex items-center mt-5">
+                    <img
+                      className="border rounded-[10px] mr-3"
+                      src="/images/plus.png"
+                      alt="profile"
+                      width="30px"
+                      height="30px"
+                    />
+                    <h4 className="text-[#EBEBEC]">Add Team</h4>
+                  </div>
+                </Link>
               </div>
             </div>
-          </div>
-          <hr className="mt-3" />
-          <div className="mt-2 w-[350px] h-[150px] overflow-y-scroll">
-            <TeamBox
-              id={id}
-              teamId={0}
-              profile={user.profile}
-              name={user.nickname}
-              onClick={() => handleUpdateTeam(0)}
-            />
-            {data.map((item) => (
-              <TeamBox
-                key={item.id}
-                id={id}
-                teamId={item.team.id}
-                profile={item.team.profile}
-                name={item.team.name}
-                onClick={() => handleUpdateTeam(item)}
-              />
-            ))}
-            <Link href="/save-team">
-              <div className="flex items-center mt-5">
-                <img
-                  className="border rounded-[10px] mr-3"
-                  src="/images/plus.png"
-                  alt="profile"
-                  width="30px"
-                  height="30px"
-                />
-                <h4 className="text-[#EBEBEC]">Add Team</h4>
-              </div>
-            </Link>
-          </div>
-        </div>
-      )}
-      {modalTeamOpen && (
-        <TeamModal
-          open={modalTeamOpen}
-          setOpen={setTeamModalOpen}
-          cancelButtonRef={cancelButtonRef}
-        />
-      )}
-      {userModalOpen && (
-        <UserModal
-          open={userModalOpen}
-          setOpen={setUserModalOpen}
-          cancelButtonRef={cancelButtonRef}
-        />
-      )}
-    </div>
+          </>
+        )}
+        {modalTeamOpen && (
+          <TeamModal
+            open={modalTeamOpen}
+            setOpen={setTeamModalOpen}
+            cancelButtonRef={cancelButtonRef}
+          />
+        )}
+        {userModalOpen && (
+          <UserModal
+            open={userModalOpen}
+            setOpen={setUserModalOpen}
+            cancelButtonRef={cancelButtonRef}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
--- apps/web/src/components/popover/workspace-popover/workspace-popover.tsx
+++ apps/web/src/components/popover/workspace-popover/workspace-popover.tsx
@@ -38,47 +38,50 @@ const WorkspacePopoverView = ({
         <ProfileBox image="/images/profile.jpg" alt="profile" />
       </div>
       {open && (
-        <div className="popover p-5 rounded-[20px] w-[350px] h-[250px] absolute bg-white shadow-md p- -translate-y-full translate-x-20">
-          <div className="flex items-center justify-between">
-            <h3 className="text-lg font-bold">DICE</h3>
-            <div className="flex items-center" onClick={handleModalOpen}>
-              <img
-                src="/images/settings.png"
-                width="18px"
-                height="18px"
-                className="mr-1"
-              />
-              <h4 className="mr-1 text-sm">Setting</h4>
+        <>
+          <div onClick={handleOpen} className="fixed inset-0 z-10" />
+          <div className="popover p-5 rounded-[20px] w-[350px] h-[250px] absolute bg-white shadow-md p- -translate-y-full translate-x-20">
+            <div className="flex items-center justify-between">
+              <h3 className="text-lg font-bold">DICE</h3>
+              <div className="flex items-center" onClick={handleModalOpen}>
+                <img
+                  src="/images/settings.png"
+                  width="18px"
+                  height="18px"
+                  className="mr-1"
+                />
+                <h4 className="mr-1 text-sm">Setting</h4>
+              </div>
+            </div>
+            <hr className="mt-3" />
+            <div className="mt-2 w-[350px] h-[150px] overflow-y-scroll">
+              {data.map((item) => (
+                <WorkspaceBox
+                  key={item.id}
+                  id={id}
+                  workspaceId={item.id}
+                  profile={item.workspace.profile}
+                  name={item.workspace.name}
+                  onClick={() => {
+                    handleUpdateWorkspace(item);
+                  }}
+                />
+              ))}
+              <Link href="/save-workspace">
+                <div className="flex items-center mt-5">
+                  <img
+                    className="border rounded-[10px] mr-3"
+                    src="/images/plus.png"
+                    alt="profile"
+                    width="30px"
+                    height="30px"
+                  />
+                  <h4 className="text-[#EBEBEC]">Add Workspace</h4>
+                </div>
+              </Link>
             </div>
           </div>
-          <hr className="mt-3" />
-          <div className="mt-2 w-[350px] h-[150px] overflow-y-scroll">
-            {data.map((item) => (
-              <WorkspaceBox
-                key={item.id}
-                id={id}
-                workspaceId={item.id}
-                profile={item.workspace.profile}
-                name={item.workspace.name}
-                onClick={() => {
-                  handleUpdateWorkspace(item);
-                }}
-              />
-            ))}
-            <Link href="/save-workspace">
-              <div className="flex items-center mt-5">
-                <img
-                  className="border rounded-[10px] mr-3"
-                  src="/images/plus.png"
-                  alt="profile"
-                  width="30px"
-                  height="30px"
-                />
-                <h4 className="text-[#EBEBEC]">Add Workspace</h4>
-              </div>
-            </Link>
-          </div>
-        </div>
+        </>
       )}
       {modalOpen && (
         <WorkspaceModal

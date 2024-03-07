62714634010cc2123a44dc9038eef2d84ca40031
rubber <rubber1926@aiaracorp.com>
2024-03-07 23:29:52 +0900

    feat(web): caleander header ui fix

--- apps/web/src/components/Calendar/calendar.tsx
+++ apps/web/src/components/Calendar/calendar.tsx
@@ -1,7 +1,18 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import "./caleander.css";
 
 const CustomCalendarView = () => {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
+  return (
+    <FullCalendar
+      headerToolbar={{
+        start: "",
+        center: "prev title next",
+        end: "",
+      }}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+    />
+  );
 };
 export default CustomCalendarView;

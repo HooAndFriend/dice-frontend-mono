3c0ad5c6793604dc9e824df60789f1d2952d2bb8
pinomaker-hoo <inhoo987654321@gmail.com>
2024-03-09 00:07:19 +0900

    feat : Add Ticket Status Button

---
+++ apps/web/src/components/Ticket/TicketStatusButton/index.tsx
@@ -0,0 +1,53 @@
+import { TicketStatus } from "@/src/type/component";
+
+interface PropsType {
+  status: TicketStatus;
+  width?: string;
+  height?: string;
+  borderRadius?: string;
+}
+
+const getButtonColor = (status: TicketStatus) => {
+  switch (status) {
+    case "REOPEN":
+      return "bg-[#FFDBD5]";
+    case "FINISH":
+      return "bg-[#CAE0F4]";
+    case "DOING":
+      return "bg-[#F7E5A9]";
+    default:
+      return "bg-black";
+  }
+};
+
+const getTextColor = (status: TicketStatus) => {
+  switch (status) {
+    case "REOPEN":
+      return "text-[#F13333]";
+    case "FINISH":
+      return "text-[#143AE1]";
+    case "DOING":
+      return "text-[#977C23]";
+    default:
+      return "text-black";
+  }
+};
+
+const TicketStatusButton = ({
+  status,
+  width = "85px",
+  height = "30px",
+  borderRadius = "6px",
+}: PropsType) => {
+  return (
+    <button
+      className={`${getButtonColor(status)} ${getTextColor(
+        status
+      )} w-[${width}] h-[${height}] rounded-[${borderRadius}]`}
+    >
+      {status}
+    </button>
+  );
+};
+
+export default TicketStatusButton;

2c12bfd5df65968b37cce168dd9e843835f21606
pinomaker-hoo <inhoo987654321@gmail.com>
2024-03-09 12:16:00 +0900

    feat : Add Search Box

--- apps/admin/src/pages/user-page/user-page.tsx
+++ apps/admin/src/pages/user-page/user-page.tsx
@@ -1,7 +1,16 @@
+// ** Component Imports
+import TitleBox from '@/src/components/TitleBox'
+import UserSearchBox from '@/src/components/UserSearchBox'
+
 interface PropsType {}
 
 const UserPageView = ({}: PropsType) => {
-  return <h1>User Page</h1>
+  return (
+    <div className="w-full px-4 mt-4">
+      <TitleBox title="사용자 관리 / 사용자 조회" text="사용자 조회" />
+      <UserSearchBox />
+    </div>
+  )
 }
 
 export default UserPageView

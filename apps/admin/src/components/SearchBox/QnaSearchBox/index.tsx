acaec0aadd8b27b1fd1f574e6362b94cb5c78264
pinomaker-hoo <inhoo987654321@gmail.com>
2024-03-10 12:05:38 +0900

    feat(admin) : Add 1:1 문의 리스트

--- apps/admin/src/components/SearchBox/QnaSearchBox/index.tsx
+++ apps/admin/src/components/SearchBox/QnaSearchBox/index.tsx
@@ -21,15 +21,15 @@ const QnaSearchBox = ({}: PropsType) => {
   return (
     <div className="h-[152px] w-full bg-white rounded-[10px] py-4 px-8 mt-4">
       <div className="flex items-center">
-        <h1 className="mr-4">가입일</h1>
+        <h1 className="mr-4">등록일</h1>
         <div className="w-[250px] mr-8">
           <Datepicker value={value} onChange={handleValueChange} />
         </div>
-        <h1 className="mr-4">최근 로그인</h1>
+        <h1 className="mr-4">답변일</h1>
         <div className="w-[250px] mr-8">
           <Datepicker value={value} onChange={handleValueChange} />
         </div>
-        <h1 className="mr-4">닉네임</h1>
+        <h1 className="mr-4">제목</h1>
         <input
           type="text"
           placeholder="닉네임"
@@ -38,21 +38,22 @@ const QnaSearchBox = ({}: PropsType) => {
       </div>
       <div className="flex items-center justify-between mt-8">
         <div className="flex items-center">
-          <h1 className="mr-8">가입 구분</h1>
+          <h1 className="mr-8">카테고리</h1>
           <input type="checkbox" className="mx-2" />
           <h1 className="mr-4">전체</h1>
           <input type="checkbox" className="mx-2" />
-          <h1 className="mr-4">일반</h1>
+          <h1 className="mr-4">회원</h1>
           <input type="checkbox" className="mx-2" />
-          <h1 className="mr-4">Microsoft</h1>
+          <h1 className="mr-4">서비스</h1>
           <input type="checkbox" className="mx-2" />
-          <h1 className="mr-4">Google</h1>
-          <input type="checkbox" className="mx-2" />
-          <h1 className="mr-4">Apple</h1>
-          <input type="checkbox" className="mx-2" />
-          <h1 className="mr-4">Github</h1>
-          <input type="checkbox" className="mx-2" />
-          <h1 className="mr-4">Twitter</h1>
+          <h1 className="mr-12">기타</h1>
+          <h1 className="mr-8">답변 여부</h1>
+          <input type="radio" className="mx-2" />
+          <h1 className="mr-4">전체</h1>
+          <input type="radio" className="mx-2" />
+          <h1 className="mr-4">답변 대기</h1>
+          <input type="radio" className="mx-2" />
+          <h1 className="mr-4">답변 완료</h1>
         </div>
         <div className="flex items-center">
           <button className="w-[90px] h-[36px] rounded-[8px] bg-[#EFEFEF] mr-4">
--- apps/admin/src/pages/qna-page/qna-page.tsx
+++ apps/admin/src/pages/qna-page/qna-page.tsx
@@ -1,7 +1,69 @@
+// ** Component Imports
+import CustomTable from '@/src/components/Table'
+import TablePagination from '@/src/components/Table/TablePagination'
+import TitleBox from '@/src/components/TitleBox'
+import QnaSearchBox from '@/src/components/SearchBox/QnaSearchBox'
+
 interface PropsType {}
 
 const QnaPageView = ({}: PropsType) => {
-  return <h1>Qna Page</h1>
+  return (
+    <div className="w-full px-4 mt-4">
+      <TitleBox title="고객센터 관리 / 1:1 문의 조회" text="1:1 문의 조회" />
+      <QnaSearchBox />
+      <div className="h-[730px] w-full bg-white rounded-[10px] py-4 px-8 mt-4">
+        <h1 className="mb-8 font-bold">문의 목록(30명)</h1>
+        <CustomTable headerData={headerData} bodyData={bodyData} />
+        <div className="flex justify-end w-full">
+          <TablePagination />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default QnaPageView
+
+const headerData = [
+  { name: '번호', size: '5%' },
+  { name: '카테고리', size: '15%' },
+  { name: '제목', size: '15%' },
+  { name: '이름', size: '15%' },
+  { name: '이메일', size: '15%' },
+  { name: '등록일', size: '15%' },
+  { name: '답변 여부', size: '5%' },
+  { name: '답변일', size: '15%' },
+]
+
+const bodyData = [
+  [
+    { name: '1', size: '5%' },
+    { name: '다이스', size: '15%' },
+    { name: '문의 드립니다!', size: '15%' },
+    { name: '이가인', size: '15%' },
+    { name: 'pino@naver.com', size: '15%' },
+    { name: '2024-01-01 23:10:12', size: '15%' },
+    { name: '답변 대기', size: '5%' },
+    { name: '2024-01-01 23:10:12', size: '15%' },
+  ],
+  [
+    { name: '1', size: '5%' },
+    { name: '다이스', size: '15%' },
+    { name: '문의 드립니다!', size: '15%' },
+    { name: '이가인', size: '15%' },
+    { name: 'pino@naver.com', size: '15%' },
+    { name: '2024-01-01 23:10:12', size: '15%' },
+    { name: '답변 대기', size: '5%' },
+    { name: '2024-01-01 23:10:12', size: '15%' },
+  ],
+  [
+    { name: '1', size: '5%' },
+    { name: '다이스', size: '15%' },
+    { name: '문의 드립니다!', size: '15%' },
+    { name: '이가인', size: '15%' },
+    { name: 'pino@naver.com', size: '15%' },
+    { name: '2024-01-01 23:10:12', size: '15%' },
+    { name: '답변 대기', size: '5%' },
+    { name: '2024-01-01 23:10:12', size: '15%' },
+  ],
+]

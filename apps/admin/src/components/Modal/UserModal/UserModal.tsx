8caa78021704cf0f3ad8b4e59a42be118dbf04b5
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-03-27 01:34:17 +0900

    feat(admin) : check user detail

--- apps/admin/src/components/Modal/UserModal/UserModal.tsx
+++ apps/admin/src/components/Modal/UserModal/UserModal.tsx
@@ -5,14 +5,34 @@ import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import CustomTable from '../../Table'
 import TablePagination from '../../Table/TablePagination'
+import { UserInfo, UserTeam, UserWorkspace } from '@/src/type/user'
+import { formatDate } from '@/src/pages/user-page'
 
 interface PropsType {
   open: boolean
+  userData: UserInfo
+  teamData: UserTeam[]
+  workspaceData: UserWorkspace[]
   cancelButtonRef: any
   setOpen: (open: boolean) => void
 }
 
-const UserModalView = ({ open, cancelButtonRef, setOpen }: PropsType) => {
+const UserModalView = ({ open, userData, teamData, workspaceData,cancelButtonRef, setOpen }: PropsType) => {
+
+  const TeamBodyData = teamData.map((team) => [
+    {name: team.team.name, size: '30%'},
+    {name: team.role, size: '20%'},
+    {name: '누군가', size: '20%'},
+    {name: formatDate(team.createdDate), size: '30%'}
+  ]);
+
+  const WorkspaceBodyData = workspaceData.map((workspace) => [
+    {name: workspace.workspace.name, size: '30%'},
+    {name: workspace.role, size: '20%'},
+    {name: '누군가', size: '20%'},
+    {name: formatDate(workspace.createdDate), size: '30%'}
+  ])
+
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -58,21 +78,21 @@ const UserModalView = ({ open, cancelButtonRef, setOpen }: PropsType) => {
                     <div className="flex items-center">
                       <div className="w-full h-[20px] flex items-center">
                         <h1 className="w-[100px] px-4 font-bold">닉네임</h1>
-                        <h1 className="text-[#696374]">babting</h1>
+                        <h1 className="text-[#696374]">{userData.user_nickname}</h1>
                       </div>
                       <div className="w-full h-[20px] flex items-center">
                         <h1 className="w-[100px] px-4 font-bold">이메일</h1>
-                        <h1 className="text-[#696374]">dlrkdls997@naver.com</h1>
+                        <h1 className="text-[#696374]">{userData.user_email}</h1>
                       </div>
                     </div>
                     <div className="flex items-center mt-[20px]">
                       <div className="w-full h-[20px] flex items-center">
                         <h1 className="w-[100px] px-4 font-bold">가입구분</h1>
-                        <h1 className="text-[#696374]">Google</h1>
+                        <h1 className="text-[#696374]">{userData.user_type}</h1>
                       </div>
                       <div className="w-full h-[20px] flex items-center">
                         <h1 className="w-[100px] px-4 font-bold">가입일</h1>
-                        <h1 className="text-[#696374]">2024-02-02 14:35:35</h1>
+                        <h1 className="text-[#696374]">{formatDate(userData.user_created_date)}</h1>
                       </div>
                     </div>
                     <div className="flex items-center mt-[20px]">
@@ -80,14 +100,14 @@ const UserModalView = ({ open, cancelButtonRef, setOpen }: PropsType) => {
                         <h1 className="w-[100px] px-4 font-bold">
                           최근 로그인
                         </h1>
-                        <h1 className="text-[#696374]">2024-02-02 14:35:35</h1>
+                        <h1 className="text-[#696374]">{formatDate(userData.user_last_login_date)}</h1>
                       </div>
                     </div>
                     <div className="w-full mt-[20px] px-4">
                       <h1 className="font-bold mb-[12px]">ㅁ 소속 팀 수</h1>
                       <CustomTable
                         headerData={headerData}
-                        bodyData={bodyData}
+                        bodyData={TeamBodyData}
                         disabledClick
                       />
                       <div className="flex justify-end w-full">
@@ -100,7 +120,7 @@ const UserModalView = ({ open, cancelButtonRef, setOpen }: PropsType) => {
                       </h1>
                       <CustomTable
                         headerData={headerWorkspaceData}
-                        bodyData={bodyData}
+                        bodyData={WorkspaceBodyData}
                         disabledClick
                       />
                       <div className="flex justify-end w-full">
@@ -142,7 +162,7 @@ const headerWorkspaceData = [
   { name: '소속일', size: '30%' },
 ]
 
-const bodyData = [
+  const bodyData = [
   [
     { name: 'DICE_DEV', size: '30%' },
     { name: 'Admin', size: '20%' },
@@ -160,5 +180,5 @@ const bodyData = [
     { name: 'Admin', size: '20%' },
     { name: 'babting', size: '20%' },
     { name: '2024-01-01 23:10:12', size: '30%' },
-  ],
-]
+    ],
+  ]
--- apps/admin/src/components/Modal/UserModal/index.tsx
+++ apps/admin/src/components/Modal/UserModal/index.tsx
@@ -1,16 +1,49 @@
+import { UserInfo, GetUserTeamResponse, GetUserWorkspaceResponse } from '@/src/type/user';
 import UserModalView from './UserModal'
 import { DialogProvider } from '@/src/context/DialogContext'
+import { useRecoilValue } from 'recoil';
+import useInput from '@/src/hooks/useInput';
+import { AuthState } from '@/src/app/auth';
+import useSWR from 'swr';
+import { Get } from '@/src/repository';
 
 interface PropsType {
-  open: boolean
-  cancelButtonRef: any
-  setOpen: (open: boolean) => void
+  open: boolean;
+  userInfo: UserInfo;
+  cancelButtonRef: any;
+  setOpen: (open: boolean) => void;
 }
 
-const UserModal = ({ open, setOpen, cancelButtonRef }: PropsType) => {
+const UserModal = ({ open, setOpen, userInfo, cancelButtonRef }: PropsType) : JSX.Element | null => {
+  const { accessToken } = useRecoilValue(AuthState);
+  const userId = userInfo?.user_id;
+  
+  const { data:team, isLoading:teamLoading, error:teamError } = useSWR(`/v1/user/team/${userId}`, async (url) => {
+    return Get<GetUserTeamResponse>(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  });
+  const { data:workspace, isLoading:wsLoading, error:wsError } = useSWR(`/v1/user/workspace/${userId}`, async (url) => {
+    return Get<GetUserWorkspaceResponse>(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  });
+
+  if (!team || !workspace) return null;
+  if (teamLoading || wsLoading) return null;
+  if (teamError || wsError) return null;
+
+
   return (
     <DialogProvider>
       <UserModalView
+        userData={userInfo}
+        teamData={team.data.data}
+        workspaceData={workspace.data.data}
         open={open}
         setOpen={setOpen}
         cancelButtonRef={cancelButtonRef}

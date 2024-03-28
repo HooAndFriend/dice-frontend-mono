05f2c2dbc011a031994fdc40a2925da72e1f0afb
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-03-28 23:59:37 +0900

    feat(admin) : check delete user list

---
+++ apps/admin/src/type/user-delete/index.d.ts
@@ -0,0 +1,24 @@
+import { CommonResponse } from "../common";
+
+export interface GetDeleteUserListResponse extends CommonResponse {
+  count: number;
+  data: DeleteUserInfo[];
+}
+
+export interface DeleteUserInfo {
+  createdDate: Date;
+  id: number;
+  email: string;
+  type: string;
+  nickname;
+  deleteDate: Date;
+}
+
+export interface DeleteUserQuery {
+  createdStartDate: string;
+  createdEndDate: string;
+  deletedStartDate: string ;
+  deletedEndDate: string;
+  nickname: string;
+  type: string[];
+}

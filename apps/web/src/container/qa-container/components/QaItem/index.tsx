a9d99a6f5b06b4a7b1c1b1e092e7248e924846a4
pinomaker-hoo <inhoo987654321@gmail.com>
2024-03-31 01:00:16 +0900

    fix(web) : Add new Button

--- apps/web/src/container/qa-container/components/QaItem/index.tsx
+++ apps/web/src/container/qa-container/components/QaItem/index.tsx
@@ -2,6 +2,9 @@
 import { IssueInfo } from "@/src/type/qa";
 
 // ** Utils Imports
+import dayjs from "dayjs";
+
+// ** Component Imports
 import QaStatusButton from "../QaStatusButton";
 import QaUserButton from "../QaUserButton";
 
@@ -16,24 +19,25 @@ const QaItem = ({ item, handleOpenQa }: PropsType) => {
       className="px-4 w-full hover:bg-blue-50 h-[100px] py-2 border-b border-solid border-[#EBEBEC]"
       onClick={() => handleOpenQa(item.id)}
     >
-      {/* <div className="w-full h-[30px] flex mb-[5px]">
-          {dayjs().diff(dayjs(item.createdDate), "hour") <= 24 && (
-            <div className="bg-[#F13333] w-[67px] h-[30px] rounded-[10px] text-white flex items-center justify-center font-spoqa text-base text-center">
-              NEW
-            </div>
-          )}
-        </div> */}
       <div className="flex w-full h-[30px] font-spoqa font-medium text-lg">
         <h1 className="font-bold">[{item.number}]</h1>
-        <h1 className="ml-2">{item.title}</h1>
+        <h1 className="ml-2 mr-4">{item.title}</h1>
+        {dayjs().diff(dayjs(item.createdDate), "hour") <= 24 && (
+          <div className="bg-[#F13333] w-[67px] h-[30px] rounded-[10px] text-white flex items-center justify-center font-spoqa text-base text-center">
+            NEW
+          </div>
+        )}
       </div>
       <div className="w-full h-[45px] flex justify-between items-end mb-2">
-        <QaUserButton
-          profile={item.worker.profile}
-          nickname={item.worker.nickname}
-          type="user"
-          qaId={item.id}
-        />
+        <div className="flex items-center">
+          <QaUserButton
+            profile={item.worker.profile}
+            nickname={item.worker.nickname}
+            type="user"
+            qaId={item.id}
+          />
+          <h1 className="ml-8">{item.dueDate}</h1>
+        </div>
         <QaStatusButton status={item.status} qaId={item.id} />
       </div>
     </div>

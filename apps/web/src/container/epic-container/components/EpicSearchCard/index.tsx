1bc27e3650cc2d7a592d183935b40f3c154ad66c
pinomaker-hoo <inhoo987654321@gmail.com>
2024-03-31 17:16:45 +0900

    fix(web) : Search Box

--- apps/web/src/container/epic-container/components/EpicSearchCard/index.tsx
+++ apps/web/src/container/epic-container/components/EpicSearchCard/index.tsx
@@ -3,12 +3,45 @@
 import { usePathname } from "next/navigation";
 
 // ** Component Imports
-import EpicSearchCardView from "./EpicSearchCard";
+import CustomSelect from "@/src/components/Input/CustomSelect";
+import CustomSearch from "@/src/components/Input/CustomSearch";
 
 const EpicSearchCard = () => {
   const pathname = usePathname();
 
-  return <EpicSearchCardView pathname={pathname} />;
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl font-bold">
+          Ticket / {pathname.split("/")[3] ? pathname.split("/")[3] : "Epic"}
+        </h1>
+      </div>
+      {pathname !== "/dashboard/epic/setting" && (
+        <div className="mt-6 h-[180px] w-full bg-white rounded-[20px] shadow-md p-4">
+          <div className="flex items-center w-full">
+            <div className="font-spoqa text-base font-bold ml-[25px] mr-[33px]">
+              Epic
+            </div>
+            <CustomSelect option="Epic" />
+            <div className="font-spoqa text-base font-bold ml-[25px] mr-[33px]">
+              Type
+            </div>
+            <CustomSelect option="Type" />
+            <div className="font-spoqa text-base font-bold ml-[25px] mr-[33px]">
+              Status
+            </div>
+            <CustomSelect option="Status" />
+          </div>
+          <div className="flex items-center w-full mt-8">
+            <div className="font-spoqa text-base font-bold ml-[25px] mr-[33px]">
+              Search
+            </div>
+            <CustomSearch />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EpicSearchCard;

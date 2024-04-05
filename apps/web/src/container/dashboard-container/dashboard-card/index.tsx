296e31d1737d40f3714ab654d90f568d7e867c81
pinomaker-hoo <inhoo987654321@gmail.com>
2024-04-06 00:03:34 +0900

    fix(web : Dashboard Card

--- apps/web/src/container/dashboard-container/dashboard-card/index.tsx
+++ apps/web/src/container/dashboard-container/dashboard-card/index.tsx
@@ -1,5 +1,5 @@
-// ** Component Imports
-import DashboardCardView from "./dashboard-card";
+// ** Next Imports
+import Image from "next/image";
 
 interface PropsType {
   width?: string;
@@ -21,15 +21,45 @@ const DashboardCard = ({
   value,
 }: PropsType) => {
   return (
-    <DashboardCardView
-      width={width}
-      height={height}
-      color={color}
-      icon={icon}
-      title={title}
-      text={text}
-      value={value}
-    />
+    <div
+      className={`mt-6 h-[${height ? height : "215px"}] w-[${
+        width ? width : "380px"
+      }] bg-white rounded-[20px] shadow-md py-4 px-8`}
+    >
+      <div
+        className={`w-[60px] h-[60px] bg-[${color}] rounded-full flex justify-center items-center`}
+      >
+        <Image src={icon} alt="title" width={30} height={30} />
+      </div>
+      <div className="mt-6">
+        <h1 className="text-[32px] font-bold">{text}</h1>
+      </div>
+      <div className="flex items-center justify-between mt-6">
+        <p className="text-[#676767]">{title}</p>
+        {value === 0 ? (
+          <div className="flex">
+            <p className={`text-[${value >= 0 ? "#FF553E" : "#22B863"}] -mr-2`}>
+              0%
+            </p>
+          </div>
+        ) : (
+          <div className="flex">
+            <Image
+              src={`${
+                value >= 0 ? "/svg/dashboard-up.svg" : "/svg/dashboard-down.svg"
+              }`}
+              width={20}
+              height={20}
+              alt="up"
+              className="mr-2"
+            />
+            <p className={`text-[${value >= 0 ? "#FF553E" : "#22B863"}] -mr-2`}>
+              {value >= 0 ? `+ ${value}%` : `- ${value * -1}%`}
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 

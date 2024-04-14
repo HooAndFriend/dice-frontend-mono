15e466c0c7a56e67bea7f0924304544c9dadf0f7
pinomaker-hoo <inhoo987654321@gmail.com>
2024-04-14 12:58:18 +0900

    fix(web) : Header Layout

--- apps/web/src/components/Dashboard/DashboardHeader/index.tsx
+++ apps/web/src/components/Dashboard/DashboardHeader/index.tsx
@@ -2,28 +2,12 @@
 
 // ** Next Imports
 import Link from "next/link";
-import Image from "next/image";
-
-// ** React Imports
-import { useEffect, useState } from "react";
-
-// ** Recoil Imports
-import { UserState } from "@/src/app";
-import { useRecoilValue } from "recoil";
 
 // ** Component Imports
-import TeamPopover from "@/src/components/Popover/TeamPopover";
 import CustomImage from "../../Image/CustomImage";
 import UserPopover from "../../Popover/UserPopover";
 
 const DashboardHeader = () => {
-  const [name, setName] = useState<string>("");
-  const { nickname } = useRecoilValue(UserState);
-
-  useEffect(() => {
-    setName(nickname);
-  }, [nickname]);
-
   return (
     <div className="h-[64px] border-b-2 border-[#EBEBEC] flex items-center justify-between">
       <Link href="/dashboard">
@@ -37,8 +21,6 @@ const DashboardHeader = () => {
       </Link>
       <div className="flex items-center mr-[30px]">
         <UserPopover />
-        {/* <TeamPopover /> */}
-        {/* <h2>{name}</h2> */}
       </div>
     </div>
   );

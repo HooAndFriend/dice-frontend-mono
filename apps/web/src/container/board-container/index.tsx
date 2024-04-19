07893de056f29b50dd1ff8b88cdb8b430eb31251
pinomaker-hoo <inhoo987654321@gmail.com>
2024-04-20 00:33:35 +0900

    feat(web) : Update Board

--- apps/web/src/container/board-container/index.tsx
+++ apps/web/src/container/board-container/index.tsx
@@ -7,15 +7,48 @@ import BoardContainerView from "./board-container";
 
 // ** Type Imports
 import { OutputData } from "@editorjs/editorjs";
+import { CommonResponse } from "@/src/type/common";
+
+// ** Service Imports
+import useSWRMutation from "swr/mutation";
+import { Put } from "@/src/repository";
+
+// ** Context Imports
+import { useDialog } from "@/src/context/DialogContext";
 
 const BoardContainer = () => {
   const [content, setContent] = useState<OutputData>();
   const [readOnly, setReadOnly] = useState<boolean>(true);
 
+  const { handleOpen } = useDialog();
+
   const handleSave = () => {
-    setReadOnly(true);
-    console.log("CONTENT : ", content);
+    updateBoard.trigger();
   };
+
+  const updateBoard = useSWRMutation(
+    "/v1/board",
+    async (url: string) =>
+      await Put<CommonResponse<void>>(url, {
+        boardId: 10,
+        title: "Hello",
+        content: JSON.stringify(content),
+      }),
+    {
+      onSuccess: ({ data }) => {
+        setReadOnly(true);
+      },
+      onError: (error) => {
+        handleOpen({
+          title: "Error",
+          message: error.response.data.message,
+          logLevel: "warn",
+          buttonText: "Close",
+          type: "alert",
+        });
+      },
+    }
+  );
 
   return (
     <BoardContainerView

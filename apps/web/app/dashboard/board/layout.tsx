4357b2436d95170ae90e609ccc63a3b273f321ea
pinomaker-hoo <inhoo987654321@gmail.com>
2024-04-19 15:19:34 +0900

    feat(web) : 임시 Board 사이드바

--- apps/web/app/dashboard/board/layout.tsx
+++ apps/web/app/dashboard/board/layout.tsx
@@ -1,7 +1,16 @@
+import BoardSidebar from "@/src/components/Dashboard/BoardSidebar";
+
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  return <>{children}</>;
+  return (
+    <div className="flex w-full h-full">
+      <BoardSidebar />
+      <div className="p-5 ml-235px" style={{ width: `calc(100% - 235px)` }}>
+        {children}
+      </div>
+    </div>
+  );
 }
---
+++ apps/web/src/components/Dashboard/BoardSidebar/index.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import CustomImage from "../../Image/CustomImage";
+
+const BoardSidebar = () => {
+  return (
+    <div className="w-[200px] bg-white border-r-2 border-[#EBEBEC] px-4 py-2">
+      <div className="flex items-center justify-between w-full">
+        <h1 className="text-[14px] font-bold">Content</h1>
+        <button className="w-[20px] font-bold text-[16px] h-[20px]">+</button>
+      </div>
+      <div className="mt-4">
+        {data.map((item) =>
+          item.children.length > 0 ? (
+            <div>
+              <div className="flex items-center mb-2" key={item.id}>
+                <div className="w-[5px] h-[5px] rounded-full bg-black mr-4" />
+                <p>{item.name}</p>
+              </div>
+              {item.children.map((_) => (
+                <div className="flex items-center mb-2 ml-4" key={_.id}>
+                  <div className="w-[5px] h-[5px] rounded-full bg-black mr-4" />
+                  <p>{_.name}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex items-center mb-2" key={item.id}>
+              <div className="w-[5px] h-[5px] rounded-full bg-black mr-4" />
+              <p>{item.name}</p>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BoardSidebar;
+
+const data = [
+  {
+    id: 1,
+    name: "기능 명세서",
+    children: [],
+  },
+  {
+    id: 2,
+    name: "WBS",
+    children: [],
+  },
+  {
+    id: 3,
+    name: "IA",
+    children: [],
+  },
+  {
+    id: 4,
+    name: "회의록",
+    children: [
+      {
+        id: 1,
+        name: "2024-04-01",
+        children: [],
+      },
+      {
+        id: 2,
+        name: "2024-04-04",
+        children: [],
+      },
+      {
+        id: 3,
+        name: "2024-04-05",
+        children: [],
+      },
+    ],
+  },
+];

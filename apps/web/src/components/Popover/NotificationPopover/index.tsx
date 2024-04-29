8b6507cf9b5a834bcac888ffeb52059a99baad2d
pinomaker-hoo <inhoo23@naver.com>
2024-04-30 00:51:48 +0900

    feat(web) : 알림 컴포넌트를 구현합니다.

--- apps/web/src/components/Popover/NotificationPopover/index.tsx
+++ apps/web/src/components/Popover/NotificationPopover/index.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-// ** Next Imports
-import { useRouter } from "next/navigation";
-
 // ** React Imports
 import { useState } from "react";
 
@@ -11,47 +8,91 @@ import CustomImage from "../../Image/CustomImage";
 import NotificationItem from "./NotificationItem";
 
 // ** Service Imports
-import { Get } from "@/src/repository";
+import { Get, Put } from "@/src/repository";
 import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
+
+// ** Type Imports
+import { GetNotificationListResponse } from "@/src/type/notification";
+import { CommonResponse } from "@/src/type/common";
+
+// ** Context Imports
+import { useDialog } from "@/src/context/DialogContext";
 
 const NotificationPopover = () => {
   const [open, setOpen] = useState<boolean>(false);
 
   const handleOpen = () => setOpen((cur) => !cur);
 
-  // const { error, isLoading, data } = useSWR("/push/v1", async (url) =>
-  //   Get<any>(url)
-  // );
+  const { handleOpen: handleDialogOpen } = useDialog();
 
-  // if (isLoading) return <div>Loading...</div>;
+  const { error, isLoading, data, mutate } = useSWR(
+    "/push/v1/notification",
+    async (url) => Get<GetNotificationListResponse>(url)
+  );
 
-  // console.log(data);
+  const readNotification = useSWRMutation(
+    "/push/v1/notification",
+    async (url: string) => await Put<CommonResponse<void>>(url),
+    {
+      onSuccess: () => {
+        mutate();
+      },
+      onError: (error) => {
+        handleDialogOpen({
+          title: "Error",
+          message: error.response.data.message,
+          logLevel: "warn",
+          buttonText: "Close",
+          type: "alert",
+        });
+      },
+    }
+  );
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div>
-      <div
-        onClick={handleOpen}
-        className="flex items-center justify-center mr-6 cursor-pointer"
-      >
-        <CustomImage
-          src="/svg/notification.svg"
-          width={25}
-          height={25}
-          alt="notification"
-        />
+      <div style={{ position: "relative" }}>
+        <div
+          onClick={handleOpen}
+          className="flex items-center justify-center mr-6 cursor-pointer"
+        >
+          <CustomImage
+            src="/svg/notification.svg"
+            width={25}
+            height={25}
+            alt="notification"
+          />
+          {!isLoading && data.data.count && (
+            <div
+              style={{ position: "absolute", top: -5, left: 15 }}
+              className="w-[15px] h-[15px] rounded-full bg-red-600 flex items-center justify-center"
+            >
+              <h1 className="text-white text-[12px]">{data.data.count}</h1>
+            </div>
+          )}
+        </div>
       </div>
       {open && (
         <>
           <div onClick={handleOpen} className="fixed inset-0 z-10" />
           <div className="popover p-5 rounded-[20px] w-[360px] h-[300px] absolute bg-white shadow-md p- translate-y-5 -translate-x-80 z-10 overflow-x-hidden overflow-y-auto">
-            <div className="flex items-center">
+            <div className="flex items-end justify-between">
               <h1>Notification</h1>
+              <p
+                className="text-gray-500 cursor-pointer text-[12px] mr-2"
+                onClick={readNotification.trigger}
+              >
+                Read All
+              </p>
             </div>
             <hr className="my-3" />
-            <NotificationItem />
-            <NotificationItem />
-            <NotificationItem />
-            <NotificationItem />
+            {!isLoading &&
+              data.data.data.map((item) => (
+                <NotificationItem key={item.id} data={item} />
+              ))}
           </div>
         </>
       )}

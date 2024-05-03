8e2092e0b000ddc9ebb5bec3a4b49f491bfdcf76
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-05-04 01:27:47 +0900

    Revert "feat(web) : 알림 컴포넌트를 구현합니다."
    This reverts commit f05d98bc43727bb2ce44645448c5d98ce8b2eae2.

--- apps/web/src/components/Popover/NotificationPopover/NotificationItem.tsx
+++ apps/web/src/components/Popover/NotificationPopover/NotificationItem.tsx
@@ -1,57 +1,28 @@
-// ** Context Imports
-import { useDialog } from "@/src/context/DialogContext";
-
-// ** Type Imports
-import { CommonResponse } from "@/src/type/common";
-import { Notification } from "@/src/type/notification";
+// ** Component Imports
+import CustomImage from "../../Image/CustomImage";
 
 // ** Utils Imports
 import dayjs from "dayjs";
 
-// ** Service Imports
-import { Patch } from "@/src/repository";
-import useSWRMutation from "swr/mutation";
-import { mutate } from "swr";
-
-interface PropsType {
-  data: Notification;
-}
-
-const NotificationItem = ({ data }: PropsType) => {
-  const { handleOpen: handleDialogOpen } = useDialog();
-
-  const readNotification = useSWRMutation(
-    `/push/v1/notification/${data.id}`,
-    async (url: string) => await Patch<CommonResponse<void>>(url),
-    {
-      onSuccess: () => {
-        mutate("/push/v1/notification");
-      },
-      onError: (error) => {
-        handleDialogOpen({
-          title: "Error",
-          message: error.response.data.message,
-          logLevel: "warn",
-          buttonText: "Close",
-          type: "alert",
-        });
-      },
-    }
-  );
-
+const NotificationItem = () => {
   return (
-    <div
-      className="flex mt-4 cursor-pointer"
-      onClick={readNotification.trigger}
-    >
+    <div className="flex mt-4">
+      <div className="flex justify-center w-[40px] h-full pt-1">
+        <CustomImage
+          src="/images/dice.png"
+          width={30}
+          height={30}
+          alt="profile"
+          className="rounded-full"
+        />
+      </div>
       <div className="w-[320px] px-2">
-        <div className="flex items-center justify-between">
-          <p className="text-[12px] font-san-bold">{data.title}</p>
-          <p className="text-[10px] text-gray-500 mt-1">
-            {dayjs(data.createdDate).format("YYYY-MM-DD HH:mm:ss")}
-          </p>
-        </div>
-        <p className="text-[12px] py-[5px]">{data.body}</p>
+        <p className="text-[12px]">
+          Dennis Nedry commented on Isla Nublar SOC2 compliance report{" "}
+        </p>
+        <p className="text-[10px] text-gray-500 mt-1">
+          {dayjs().format("YYYY-MM-DD HH:mm:ss")}
+        </p>
       </div>
     </div>
   );
--- apps/web/src/components/Popover/NotificationPopover/index.tsx
+++ apps/web/src/components/Popover/NotificationPopover/index.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+// ** Next Imports
+import { useRouter } from "next/navigation";
+
 // ** React Imports
 import { useState } from "react";
 
@@ -8,91 +11,47 @@ import CustomImage from "../../Image/CustomImage";
 import NotificationItem from "./NotificationItem";
 
 // ** Service Imports
-import { Get, Put } from "@/src/repository";
+import { Get } from "@/src/repository";
 import useSWR from "swr";
-import useSWRMutation from "swr/mutation";
-
-// ** Type Imports
-import { GetNotificationListResponse } from "@/src/type/notification";
-import { CommonResponse } from "@/src/type/common";
-
-// ** Context Imports
-import { useDialog } from "@/src/context/DialogContext";
 
 const NotificationPopover = () => {
   const [open, setOpen] = useState<boolean>(false);
 
   const handleOpen = () => setOpen((cur) => !cur);
 
-  const { handleOpen: handleDialogOpen } = useDialog();
+  // const { error, isLoading, data } = useSWR("/push/v1", async (url) =>
+  //   Get<any>(url)
+  // );
 
-  const { error, isLoading, data, mutate } = useSWR(
-    "/push/v1/notification",
-    async (url) => Get<GetNotificationListResponse>(url)
-  );
+  // if (isLoading) return <div>Loading...</div>;
 
-  const readNotification = useSWRMutation(
-    "/push/v1/notification",
-    async (url: string) => await Put<CommonResponse<void>>(url),
-    {
-      onSuccess: () => {
-        mutate();
-      },
-      onError: (error) => {
-        handleDialogOpen({
-          title: "Error",
-          message: error.response.data.message,
-          logLevel: "warn",
-          buttonText: "Close",
-          type: "alert",
-        });
-      },
-    }
-  );
-
-  if (isLoading) return <div>Loading...</div>;
+  // console.log(data);
 
   return (
     <div>
-      <div style={{ position: "relative" }}>
-        <div
-          onClick={handleOpen}
-          className="flex items-center justify-center mr-6 cursor-pointer"
-        >
-          <CustomImage
-            src="/svg/notification.svg"
-            width={25}
-            height={25}
-            alt="notification"
-          />
-          {!isLoading && data.data.count && (
-            <div
-              style={{ position: "absolute", top: -5, left: 15 }}
-              className="w-[15px] h-[15px] rounded-full bg-red-600 flex items-center justify-center"
-            >
-              <h1 className="text-white text-[12px]">{data.data.count}</h1>
-            </div>
-          )}
-        </div>
+      <div
+        onClick={handleOpen}
+        className="flex items-center justify-center mr-6 cursor-pointer"
+      >
+        <CustomImage
+          src="/svg/notification.svg"
+          width={25}
+          height={25}
+          alt="notification"
+        />
       </div>
       {open && (
         <>
           <div onClick={handleOpen} className="fixed inset-0 z-10" />
           <div className="popover p-5 rounded-[20px] w-[360px] h-[300px] absolute bg-white shadow-md p- translate-y-5 -translate-x-80 z-10 overflow-x-hidden overflow-y-auto">
-            <div className="flex items-end justify-between">
+            <div className="flex items-center">
               <h1>Notification</h1>
-              <p
-                className="text-gray-500 cursor-pointer text-[12px] mr-2"
-                onClick={readNotification.trigger}
-              >
-                Read All
-              </p>
             </div>
             <hr className="my-3" />
-            {!isLoading &&
-              data.data.data.map((item) => (
-                <NotificationItem key={item.id} data={item} />
-              ))}
+            <NotificationItem />
+            <NotificationItem />
+            <NotificationItem />
+            <NotificationItem />
           </div>
         </>
       )}

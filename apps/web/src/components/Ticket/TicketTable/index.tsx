fb7f521c8265e31607596af72b2d22bbb6c7e90f
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-05-04 01:27:47 +0900

    Revert "feat(web) : 티켓 테이블에서 권한이 없을 경우 생성 UI를 노출하지 않습니다."
    This reverts commit f249f2ae057dcd7b922c904c21e3f88f7b249575.

--- apps/web/src/components/Ticket/TicketTable/index.tsx
+++ apps/web/src/components/Ticket/TicketTable/index.tsx
@@ -1,7 +1,3 @@
-// ** Recoil Imports
-import { WorkspaceState } from "@/src/app";
-import { useRecoilValue } from "recoil";
-
 // ** Component Imports
 import TicketAddItem from "../TicketAddItem";
 import TicketHeader from "../TicketHeader";
@@ -26,8 +22,6 @@ interface PropsType {
 }
 
 const TicketTable = ({ word, handleClick, data, onDragEnd }: PropsType) => {
-  const { role } = useRecoilValue(WorkspaceState);
-
   return (
     <div className="mt-[44px] h-[564px] overflow-auto w-full bg-white rounded-[20px] shadow-md p-[24px]s">
       <TicketHeader isEpic={false} />
@@ -63,7 +57,7 @@ const TicketTable = ({ word, handleClick, data, onDragEnd }: PropsType) => {
           )}
         </Droppable>
       </DragDropContext>
-      {role !== "VIEWER" && <TicketAddItem />}
+      <TicketAddItem />
     </div>
   );
 };

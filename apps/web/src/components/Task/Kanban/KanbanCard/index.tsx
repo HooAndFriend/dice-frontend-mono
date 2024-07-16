21a1539e0c5d10d5f4f92c3c68afbb7b681b47c4
pinomaker-hoo <inhoo23@naver.com>
2024-07-17 01:33:55 +0900

    feat(web) : Kanban 디자인을 합니다.

---
+++ apps/web/src/components/Task/Kanban/KanbanCard/index.tsx
@@ -0,0 +1,48 @@
+import { Ticket } from "@/src/type/ticket";
+import TicketSettingButton from "../../Ticket/TicketSettingButton";
+import TicketUserButton from "../../Ticket/TicketUserButton";
+import dayjs from "dayjs";
+import CustomImage from "@/src/components/Image/CustomImage";
+
+interface PropsType {
+  data: Ticket;
+}
+
+const KanbanCard = ({ data }: PropsType) => {
+  return (
+    <div className="mt-[12px] w-full rounded-[8px] border px-[16px] py-[12px] border-[#E1E3E8] bg-white flex flex-col justify-between">
+      <h1 className="font-bold text-[16px]">{data.name}</h1>
+      <p className="text-[12px] text-gray-400 my-[8px]">
+        Details about this item can go here but are truncated after a certain
+        length.
+      </p>
+      <div className="my-[8px] flex items-center space-x-4">
+        <CustomImage
+          width={24}
+          height={24}
+          src="/svg/calendar.svg"
+          className=""
+          alt="calendar"
+        />
+        <h1>{data.dueDate ? dayjs(data.dueDate).format("YYYY-MM-DD") : "-"}</h1>
+      </div>
+      <div className="flex items-center justify-between mt-[24px]">
+        <div className="flex items-center justify-between">
+          <TicketSettingButton data={data} isText={false} />
+          <p className="text-[12px] pl-[8px]">{data.code}</p>
+        </div>
+        <TicketUserButton
+          profile={data.worker ? data.worker.profile : "/images/dice.png"}
+          nickname={data.worker ? data.worker.nickname : "-"}
+          email={data.worker ? data.worker.email : "-"}
+          userId={data.worker?.userId}
+          type="user"
+          ticketId={data.ticketId}
+          isNickname={false}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default KanbanCard;
--- apps/web/src/container/kanban-container/kanban-container.tsx
+++ apps/web/src/container/kanban-container/kanban-container.tsx
@@ -3,6 +3,7 @@
 import CustomSearch from "@/src/components/Input/CustomSearch";
 import TicketStatusSelectFilter from "@/src/components/Task/Common/Filter/StatusFilter";
 import TicketTypeSelectFilter from "@/src/components/Task/Common/Filter/TypeFilter";
+import KanbanCard from "@/src/components/Task/Kanban/KanbanCard";
 import TicketSettingButton from "@/src/components/Task/Ticket/TicketSettingButton";
 import TicketStatusButton from "@/src/components/Task/Ticket/TicketStatusButton";
 import TicketUserButton from "@/src/components/Task/Ticket/TicketUserButton";
@@ -36,49 +37,23 @@ const KanbanContainerView = ({ data }: PropsType) => {
           <h1 className="text-[18px] font-san-medium">Total Ticket : {0}</h1>
         </div>
       </div>
-      <div className="h-[92%] py-[24px]">
-        <div className="flex w-full h-full bg-white border rounded-lg scrollbar-thumb-slate-700 scrollbar-track-slate-300 px-[2%] py-[24px]">
-          <div className="flex flex-wrap w-full">
-            {statusList.map((item) => (
-              <div className="flex-1 h-full px-[12px]">
-                <div className="w-full h-full bg-[#F1F2F5] px-[12px] py-[12px]">
-                  <h1>{item}</h1>
-                  {data
-                    .filter((_) => _.status === item)
-                    .map((_) => (
-                      <div className="mt-[12px] w-full h-[140px] border-2 p-[8px] border-[#E1E3E8] bg-white flex flex-col justify-between">
-                        <div>
-                          <div className="flex items-center">
-                            <TicketSettingButton data={_} isText={false} />
-                            <p className="pl-[8px]">{_.code}</p>
-                          </div>
-                          <div className="pt-[12px]">
-                            <h1>{_.name}</h1>
-                          </div>
-                        </div>
-                        <div className="flex items-center justify-between mt-auto">
-                          <TicketStatusButton
-                            ticketId={_.ticketId}
-                            status={_.status}
-                          />
-                          <TicketUserButton
-                            profile={
-                              _.worker ? _.worker.profile : "/images/dice.png"
-                            }
-                            nickname={_.worker ? _.worker.nickname : "-"}
-                            email={_.worker ? _.worker.email : "-"}
-                            userId={_.worker?.userId}
-                            type="user"
-                            ticketId={_.ticketId}
-                            isNickname={false}
-                          />
-                        </div>
-                      </div>
-                    ))}
-                </div>
+      <div className="h-[92%] py-[24px] w-full overflow-x-auto">
+        <div className="flex space-x-2 w-max">
+          {statusList.map((item) => (
+            <div className="w-[300px] flex-shrink-0" key={item}>
+              <div className="flex items-center justify-between px-[12px]">
+                <h1>{item}</h1>
+                <h1>+</h1>
               </div>
-            ))}
-          </div>
+              <div className="p-2">
+                {data
+                  .filter((_) => _.status === item)
+                  .map((ticket) => (
+                    <KanbanCard data={ticket} />
+                  ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>

a93b1cb43c7c737ae44ab5b4fbb4cd9ea3a904b7
pinomaker-hoo <inhoo23@naver.com>
2024-07-18 01:25:17 +0900

    feat(web) : Sprint의 Sample UI 구성

---
+++ apps/web/src/components/Task/Sprint/SprintArea/index.tsx
@@ -0,0 +1,37 @@
+import { Ticket } from "@/src/type/ticket";
+import TicketSettingButton from "../../Ticket/TicketSettingButton";
+import TicketUserButton from "../../Ticket/TicketUserButton";
+import dayjs from "dayjs";
+import CustomImage from "@/src/components/Image/CustomImage";
+import { useState } from "react";
+import TicketTable from "../../Ticket/TicketTable";
+
+interface PropsType {
+  data: Ticket[];
+}
+
+const SprintArea = ({ data }: PropsType) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <div className="w-full bg-gray-200 rounded-[12px]">
+      <div className="w-full h-[70px] flex items-center px-[12px] space-x-4">
+        <h1 className="cursor-pointer " onClick={() => setOpen((cur) => !cur)}>
+          {">"}
+        </h1>
+        <h1>스프린트 1</h1>
+        <h1>
+          {dayjs().format("YYYY-MM-DD")} - {dayjs().format("YYYY-MM-DD")}
+        </h1>
+        <h1>({data.length}개 이슈)</h1>
+      </div>
+      {open && (
+        <div className="w-full h-full p-[12px]">
+          <TicketTable handleClick={() => {}} word={""} data={data} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SprintArea;

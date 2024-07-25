31ab2c032146f127f2dac2a7f5c03699110bd4f1
pinomaker-hoo <inhoo23@naver.com>
2024-07-25 23:16:26 +0900

    feat(web) : Ticket Container 변경

--- apps/web/app/dashboard/task/table/page.tsx
+++ apps/web/app/dashboard/task/table/page.tsx
@@ -1,7 +1,121 @@
-import TicketConatiner from "@/src/container/ticket-container";
+"use client";
+
+// ** React Imports
+import { useEffect, useState } from "react";
+
+// ** Component Imports
+import TicketContainer from "@/src/container/ticket-container";
+
+// ** Service Imports
+import { Get, Patch } from "@/src/repository";
+import useSWRMutation from "swr/mutation";
+import useSWR from "swr";
+
+// ** Type Imports
+import { GetTicketListResponse } from "@/src/type/ticket";
+import { CommonResponse } from "@/src/type/common";
+
+// ** Context Imports
+import { useDialog } from "@/src/context/DialogContext";
+import { WorkspaceUser } from "@/src/type/workspace";
+import { EpicStatus } from "@/src/type/epic";
 
 const TicketPage = () => {
-  return <TicketConatiner />;
+  const [word, setWord] = useState<string>("");
+  const [selectedEpicIds, setSelectedEpicIds] = useState<number[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<EpicStatus[]>([]);
+  const [selectedTypeIds, setSelectedTypeIds] = useState<number[]>([]);
+
+  const [ticketId, setTicketId] = useState<number>(0);
+  const [checkedList, setCheckedList] = useState<WorkspaceUser[]>([]);
+
+  const [enabled, setEnabled] = useState<boolean>(false);
+
+  const { handleOpen } = useDialog();
+
+  const {
+    data,
+    error,
+    isLoading,
+    mutate: handleTicketRefetch,
+  } = useSWR("/v1/ticket", async (url) => Get<GetTicketListResponse>(url));
+
+  const updateOrder = useSWRMutation(
+    "/v1/ticket/order",
+    async (
+      url: string,
+      { arg }: { arg: { ticketId: number; targetTicketId: number } }
+    ) => await Patch<CommonResponse<void>>(url, arg),
+    {
+      onSuccess: ({ data }) => {
+        handleTicketRefetch();
+      },
+      onError: (error) => {
+        handleOpen({
+          title: "Error",
+          message: error.response.data.message,
+          logLevel: "warn",
+          buttonText: "Close",
+          type: "alert",
+        });
+      },
+    }
+  );
+
+  const handleEpicSelectFilter = (epicId: number) => {
+    if (selectedEpicIds.includes(epicId)) {
+      setSelectedEpicIds((c) => c.filter((id) => id !== epicId));
+    } else {
+      setSelectedEpicIds((c) => [...c, epicId]);
+    }
+  };
+
+  const handleStatusSelectFilter = (status: EpicStatus) => {
+    if (selectedStatus.includes(status)) {
+      setSelectedStatus((c) => c.filter((s) => s !== status));
+    } else {
+      setSelectedStatus((c) => [...c, status]);
+    }
+  };
+
+  const handleTypeSelectFilter = (typeId: number) => {
+    if (selectedTypeIds.includes(typeId)) {
+      setSelectedTypeIds((c) => c.filter((id) => id !== typeId));
+    } else {
+      setSelectedTypeIds((c) => [...c, typeId]);
+    }
+  };
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (error || !enabled) return;
+
+  return (
+    <TicketContainer
+      ticketId={ticketId}
+      selectedStatus={selectedStatus}
+      selectedTypeIds={selectedTypeIds}
+      selectedEpicIds={selectedEpicIds}
+      data={isLoading ? [] : data.data.data}
+      ticketCount={isLoading ? 0 : data.data.count}
+      word={word}
+      checkedList={checkedList}
+      handleTypeSelectFilter={handleTypeSelectFilter}
+      handleStatusSelectFilter={handleStatusSelectFilter}
+      handleEpicSelectFilter={handleEpicSelectFilter}
+      handleWord={(e) => setWord(e.target.value)}
+      setCheckedList={setCheckedList}
+      setTicketId={setTicketId}
+      isLoading={isLoading}
+    />
+  );
 };
 
 export default TicketPage;
--- apps/web/src/container/ticket-container/index.tsx
+++ apps/web/src/container/ticket-container/index.tsx
@@ -1,121 +1,117 @@
-"use client";
-
-// ** React Imports
-import { useEffect, useState } from "react";
-
 // ** Component Imports
-import TicketContainerView from "./ticket-container";
-
-// ** Service Imports
-import { Get, Patch } from "@/src/repository";
-import useSWRMutation from "swr/mutation";
-import useSWR from "swr";
+import TicketCard from "@/src/components/Task/Ticket/TicketCard";
+import TicketTable from "@/src/components/Task/Ticket/TicketTable";
+import TicketTableSkeleton from "@/src/components/Task/Ticket/TicketTable/TicketTableSkeleton";
+import CustomSearch from "@/src/components/Input/CustomSearch";
+import TicketStatusSelectFilter from "@/src/components/Task/Common/Filter/StatusFilter";
+import TicketTypeSelectFilter from "@/src/components/Task/Common/Filter/TypeFilter";
+import UserSelectBox from "@/src/components/UserSelectBox";
 
 // ** Type Imports
-import { GetTicketListResponse } from "@/src/type/ticket";
-import { CommonResponse } from "@/src/type/common";
-
-// ** Context Imports
-import { useDialog } from "@/src/context/DialogContext";
+import { Ticket } from "@/src/type/ticket";
 import { WorkspaceUser } from "@/src/type/workspace";
 import { EpicStatus } from "@/src/type/epic";
 
-const TicketConatiner = () => {
-  const [word, setWord] = useState<string>("");
-  const [selectedEpicIds, setSelectedEpicIds] = useState<number[]>([]);
-  const [selectedStatus, setSelectedStatus] = useState<EpicStatus[]>([]);
-  const [selectedTypeIds, setSelectedTypeIds] = useState<number[]>([]);
+interface PropsType {
+  ticketId: number;
+  data: Ticket[];
+  word: string;
+  ticketCount: number;
+  isLoading: boolean;
+  checkedList: WorkspaceUser[];
+  selectedEpicIds: number[];
+  selectedStatus: EpicStatus[];
+  selectedTypeIds: number[];
+  handleTypeSelectFilter: (typeId: number) => void;
+  handleStatusSelectFilter: (status: EpicStatus) => void;
+  handleEpicSelectFilter: (epicId: number) => void;
+  setCheckedList: (list: WorkspaceUser[]) => void;
+  setTicketId: (id: number) => void;
+  handleWord: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-  const [ticketId, setTicketId] = useState<number>(0);
-  const [checkedList, setCheckedList] = useState<WorkspaceUser[]>([]);
-
-  const [enabled, setEnabled] = useState<boolean>(false);
-
-  const { handleOpen } = useDialog();
-
-  const {
-    data,
-    error,
-    isLoading,
-    mutate: handleTicketRefetch,
-  } = useSWR("/v1/ticket", async (url) => Get<GetTicketListResponse>(url));
-
-  const updateOrder = useSWRMutation(
-    "/v1/ticket/order",
-    async (
-      url: string,
-      { arg }: { arg: { ticketId: number; targetTicketId: number } }
-    ) => await Patch<CommonResponse<void>>(url, arg),
-    {
-      onSuccess: ({ data }) => {
-        handleTicketRefetch();
-      },
-      onError: (error) => {
-        handleOpen({
-          title: "Error",
-          message: error.response.data.message,
-          logLevel: "warn",
-          buttonText: "Close",
-          type: "alert",
-        });
-      },
-    }
-  );
-
-  const handleEpicSelectFilter = (epicId: number) => {
-    if (selectedEpicIds.includes(epicId)) {
-      setSelectedEpicIds((c) => c.filter((id) => id !== epicId));
-    } else {
-      setSelectedEpicIds((c) => [...c, epicId]);
-    }
-  };
-
-  const handleStatusSelectFilter = (status: EpicStatus) => {
-    if (selectedStatus.includes(status)) {
-      setSelectedStatus((c) => c.filter((s) => s !== status));
-    } else {
-      setSelectedStatus((c) => [...c, status]);
-    }
-  };
-
-  const handleTypeSelectFilter = (typeId: number) => {
-    if (selectedTypeIds.includes(typeId)) {
-      setSelectedTypeIds((c) => c.filter((id) => id !== typeId));
-    } else {
-      setSelectedTypeIds((c) => [...c, typeId]);
-    }
-  };
-
-  useEffect(() => {
-    const animation = requestAnimationFrame(() => setEnabled(true));
-
-    return () => {
-      cancelAnimationFrame(animation);
-      setEnabled(false);
-    };
-  }, []);
-
-  if (error || !enabled) return;
-
+const TicketContainer = ({
+  ticketId,
+  data,
+  word,
+  selectedStatus,
+  selectedTypeIds,
+  isLoading,
+  ticketCount,
+  setTicketId,
+  handleWord,
+  checkedList,
+  setCheckedList,
+  handleTypeSelectFilter,
+  handleStatusSelectFilter,
+}: PropsType) => {
   return (
-    <TicketContainerView
-      ticketId={ticketId}
-      selectedStatus={selectedStatus}
-      selectedTypeIds={selectedTypeIds}
-      selectedEpicIds={selectedEpicIds}
-      data={isLoading ? [] : data.data.data}
-      ticketCount={isLoading ? 0 : data.data.count}
-      word={word}
-      checkedList={checkedList}
-      handleTypeSelectFilter={handleTypeSelectFilter}
-      handleStatusSelectFilter={handleStatusSelectFilter}
-      handleEpicSelectFilter={handleEpicSelectFilter}
-      handleWord={(e) => setWord(e.target.value)}
-      setCheckedList={setCheckedList}
-      setTicketId={setTicketId}
-      isLoading={isLoading}
-    />
+    <div className="w-full h-full">
+      <div className="flex items-center justify-between pt-8 h-[8%]">
+        <div className="flex items-center space-x-4">
+          <CustomSearch width="200px" value={word} onChange={handleWord} />
+          <TicketStatusSelectFilter
+            selectedStatus={selectedStatus}
+            handleEpicSelectFilter={handleStatusSelectFilter}
+          />
+          <TicketTypeSelectFilter
+            selectedTypeIds={selectedTypeIds}
+            handleTypeSelectFilter={handleTypeSelectFilter}
+          />
+          <UserSelectBox
+            checkedList={checkedList}
+            setCheckedList={setCheckedList}
+          />
+        </div>
+        <div className="flex items-center">
+          <h1 className="text-[18px] font-san-medium">
+            Total Ticket : {ticketCount}
+          </h1>
+        </div>
+      </div>
+      <div className={`${ticketId !== 0 && "flex"} h-[92%] py-[24px]`}>
+        <div style={{ width: ticketId !== 0 ? "65%" : "100%", height: "100%" }}>
+          {isLoading ? (
+            <TicketTableSkeleton />
+          ) : (
+            <TicketTable
+              handleClick={setTicketId}
+              word={word}
+              data={data
+                .filter((item) => item.name.includes(word))
+                .filter((item) =>
+                  checkedList.length === 0
+                    ? true
+                    : checkedList.some(
+                        (_) => _.user.userId === item.worker?.userId
+                      )
+                )
+                .filter((item) =>
+                  selectedStatus.length === 0
+                    ? true
+                    : selectedStatus.includes(item.status)
+                )
+                .filter((item) =>
+                  selectedTypeIds.length === 0
+                    ? true
+                    : selectedTypeIds.includes(
+                        item.ticketSetting?.ticketSettingId
+                      )
+                )}
+            />
+          )}
+        </div>
+        {ticketId !== 0 && (
+          <div className="w-[35%] pl-8">
+            <TicketCard
+              ticketId={ticketId}
+              handleClose={() => setTicketId(0)}
+            />
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
-export default TicketConatiner;
+export default TicketContainer;

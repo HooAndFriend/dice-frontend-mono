27a4babe47f576294c0c7146b7957cb3c69dd550
pinomaker-hoo <inhoo23@naver.com>
2024-08-09 18:55:04 +0900

    feat(web) : Kanban Container 구조 재구성

--- apps/web/app/dashboard/task/kanban/page.tsx
+++ apps/web/app/dashboard/task/kanban/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 // ** Component Imports
 import KanbanContainer from "@/src/container/kanban-container";
 
+// ** Service Imports
+import useSWR from "swr";
+import { Get } from "@/src/repository";
+
+// ** Type Imports
+import { GetTicketListResponse } from "@/src/type/ticket";
+
 const KanbanPage = () => {
-  return <KanbanContainer />;
+  const {
+    data,
+    error,
+    isLoading,
+    mutate: handleTicketRefetch,
+  } = useSWR("/v1/ticket", async (url) => Get<GetTicketListResponse>(url));
+
+  if (error) return;
+
+  return <KanbanContainer data={isLoading ? [] : data.data.data} />;
 };
 
 export default KanbanPage;

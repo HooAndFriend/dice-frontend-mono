5a658ef6132517360ac1d99115c96bf06c99c3bb
pinomaker-hoo <inhoo23@naver.com>
2024-08-18 01:01:47 +0900

    Update page.tsx

--- apps/web/app/[workspaceUid]/dashboard/task/epic/page.tsx
+++ apps/web/app/[workspaceUid]/dashboard/task/epic/page.tsx
@@ -1,103 +1,113 @@
-"use client";
+'use client'
 // ** React Imports
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from 'react'
 
 // ** Component Imports
 
 // ** Service Imports
-import useSWR, { mutate } from "swr";
-import { Get, Patch } from "@/src/repository";
-import useSWRMutation from "swr/mutation";
+import useSWR, { mutate } from 'swr'
+import { Get, Patch } from '@/src/repository'
+import useSWRMutation from 'swr/mutation'
 
 // ** Utils Imports
-import { DropResult } from "react-beautiful-dnd";
+import { DropResult } from 'react-beautiful-dnd'
 
 // ** Type Imports
-import {
-  EpicStatus,
-  GetEpicListResponse,
-  SelectContent,
-} from "@/src/type/epic";
-import { CommonResponse } from "@/src/type/common";
+import { EpicStatus, GetEpicListResponse, SelectContent } from '@/src/type/epic'
+import { CommonResponse } from '@/src/type/common'
 
 // ** Context Imports
-import { useDialog } from "@/src/context/DialogContext";
-import { WorkspaceUser } from "@/src/type/workspace";
-import EpicContainer from "@/src/container/epic-container";
+import { useDialog } from '@/src/context/DialogContext'
+import { WorkspaceUser } from '@/src/type/workspace'
+import EpicContainer from '@/src/container/epic-container'
+import { useSearchParams } from 'next/navigation'
 
 const EpicConatiner = () => {
-  const [word, setWord] = useState<string>("");
+  const [word, setWord] = useState<string>('')
   const [selectContent, setSelectContent] = useState<SelectContent>({
     id: 0,
-    type: "EPIC",
-  });
-  const [selectedTypeIds, setSelectedTypeIds] = useState<number[]>([]);
-  const [selectedStatus, setSelectedStatus] = useState<EpicStatus[]>([]);
-  const [checkedList, setCheckedList] = useState<WorkspaceUser[]>([]);
+    type: 'EPIC',
+  })
 
-  const [enabled, setEnabled] = useState<boolean>(false);
+  const searchParams = useSearchParams()
 
-  const { handleOpen } = useDialog();
+  const [selectedTypeIds, setSelectedTypeIds] = useState<number[]>([])
+  const [selectedStatus, setSelectedStatus] = useState<EpicStatus[]>([])
+  const [checkedList, setCheckedList] = useState<WorkspaceUser[]>([])
 
-  const { data, error, isLoading } = useSWR("/v1/epic", async (url) =>
-    Get<GetEpicListResponse>(url)
-  );
+  const [enabled, setEnabled] = useState<boolean>(false)
+
+  const { handleOpen } = useDialog()
+
+  const { data, error, isLoading } = useSWR('/v1/epic', async (url) =>
+    Get<GetEpicListResponse>(url),
+  )
 
   const updateEpicOrder = useSWRMutation(
-    "/v1/epic/order",
+    '/v1/epic/order',
     async (
       url: string,
-      { arg }: { arg: { epicId: number; targetEpicId: number } }
+      { arg }: { arg: { epicId: number; targetEpicId: number } },
     ) => await Patch<CommonResponse<void>>(url, arg),
     {
       onSuccess: ({ data }) => {
-        mutate("/v1/epic");
+        mutate('/v1/epic')
       },
       onError: (error) => {
         handleOpen({
-          title: "Error",
+          title: 'Error',
           message: error.response.data.message,
-          logLevel: "warn",
-          buttonText: "Close",
-          type: "alert",
-        });
+          logLevel: 'warn',
+          buttonText: 'Close',
+          type: 'alert',
+        })
       },
-    }
-  );
+    },
+  )
 
   const handleTypeSelectFilter = (id: number) => {
     if (selectedTypeIds.includes(id)) {
-      setSelectedTypeIds((prev) => prev.filter((v) => v !== id));
+      setSelectedTypeIds((prev) => prev.filter((v) => v !== id))
     } else {
-      setSelectedTypeIds((prev) => [...prev, id]);
+      setSelectedTypeIds((prev) => [...prev, id])
     }
-  };
+  }
 
   const handleStatusSelectFilter = (status: EpicStatus) => {
     if (selectedStatus.includes(status)) {
-      setSelectedStatus((prev) => prev.filter((v) => v !== status));
+      setSelectedStatus((prev) => prev.filter((v) => v !== status))
     } else {
-      setSelectedStatus((prev) => [...prev, status]);
+      setSelectedStatus((prev) => [...prev, status])
     }
-  };
+  }
 
   const onDragEnd = ({ source, destination }: DropResult) => {
     updateEpicOrder.trigger({
       epicId: source.index,
       targetEpicId: destination.index,
-    });
-  };
+    })
+  }
 
   useEffect(() => {
-    const animation = requestAnimationFrame(() => setEnabled(true));
+    const animation = requestAnimationFrame(() => setEnabled(true))
 
     return () => {
-      cancelAnimationFrame(animation);
-      setEnabled(false);
-    };
-  }, []);
+      cancelAnimationFrame(animation)
+      setEnabled(false)
+    }
+  }, [])
 
-  if (error || !enabled) return;
+  useEffect(() => {
+    const ticketId = searchParams.get('ticketId')
+    if (ticketId) {
+      setSelectContent({
+        id: Number(ticketId),
+        type: 'TICKET',
+      })
+    }
+  }, [])
+
+  if (error || !enabled) return
 
   return (
     <EpicContainer
@@ -116,7 +126,7 @@ const EpicConatiner = () => {
       onDragEnd={onDragEnd}
       isLoading={isLoading}
     />
-  );
-};
+  )
+}
 
-export default EpicConatiner;
+export default EpicConatiner

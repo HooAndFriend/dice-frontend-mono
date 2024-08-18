5eb01bc07a43700f5d0c4109264035095973daaf
pinomaker-hoo <inhoo23@naver.com>
2024-08-18 21:50:12 +0900

    feat(web) : Logo 클릭 시 이동 URL 변경

--- apps/web/src/components/Dashboard/DashboardHeader/index.tsx
+++ apps/web/src/components/Dashboard/DashboardHeader/index.tsx
@@ -7,12 +7,15 @@ import Link from 'next/link'
 import CustomImage from '../../Image/CustomImage'
 import UserPopover from '../../Popover/UserPopover'
 import NotificationPopover from '../../Popover/NotificationPopover'
+import { useRecoilValue } from 'recoil'
+import { WorkspaceState } from '@/src/app'
 
 const DashboardHeader = () => {
+  const { uuid } = useRecoilValue(WorkspaceState)
   return (
     <div className="h-[60px] border-b-2 border-[#EBEBEC] flex items-center justify-between">
       <div className="flex items-center">
-        <Link href="/dashboard">
+        <Link href={`/${uuid}/dashboard`}>
           <CustomImage
             src="/images/logo.png"
             width={110}

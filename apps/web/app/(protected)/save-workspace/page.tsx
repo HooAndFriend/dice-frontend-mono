61b2a1d508613e605abc90adc4af78c0a5273cb2
pinomaker-hoo <inhoo23@naver.com>
2024-08-19 00:42:44 +0900

    feat(web) : Workspace ∞â¥∞ä▒ φ¢ä URL δ│ÇΩ▓╜

--- apps/web/app/(protected)/save-workspace/page.tsx
+++ apps/web/app/(protected)/save-workspace/page.tsx
@@ -1,29 +1,31 @@
-"use client";
+'use client'
 
 // ** Next Imports
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation'
 
 // ** React Imports
-import { KeyboardEvent } from "react";
+import { KeyboardEvent } from 'react'
 
 // ** Service Imports
-import useSWRMutation from "swr/mutation";
-import { Post } from "@/src/repository";
+import useSWRMutation from 'swr/mutation'
+import { Post } from '@/src/repository'
 
 // ** Component Imports
-import SaveWorkspaceContainer from "@/src/container/save-workspace-container";
+import SaveWorkspaceContainer from '@/src/container/save-workspace-container'
 
 // ** Utils Imports
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
 // ** Type Imports
-import { CommonResponse } from "@/src/type/common";
-import { SaveWorkspaceParam } from "@/src/type/workspace";
-import { saveWorksapceSchema } from "@/src/schema/workspace";
+import { CommonResponse } from '@/src/type/common'
+import { SaveWorkspaceParam } from '@/src/type/workspace'
+import { saveWorksapceSchema } from '@/src/schema/workspace'
 
 // ** Context Imports
-import { useDialog } from "@/src/context/DialogContext";
+import { useDialog } from '@/src/context/DialogContext'
+import { useRecoilValue } from 'recoil'
+import { WorkspaceState } from '@/src/app'
 
 export default function Signup(): JSX.Element {
   const {
@@ -35,50 +37,51 @@ export default function Signup(): JSX.Element {
   } = useForm<SaveWorkspaceParam>({
     resolver: zodResolver(saveWorksapceSchema),
     defaultValues: {
-      name: "",
+      name: '',
       profile:
-        "https://firebasestorage.googleapis.com/v0/b/dice-dev-a5b63.appspot.com/o/images%2FIMG_6159.jpg?alt=media&token=450c0181-8826-4856-b611-509712872450",
-      comment: "",
+        'https://firebasestorage.googleapis.com/v0/b/dice-dev-a5b63.appspot.com/o/images%2FIMG_6159.jpg?alt=media&token=450c0181-8826-4856-b611-509712872450',
+      comment: '',
     },
-  });
+  })
 
-  const { handleOpen } = useDialog();
+  const { uuid } = useRecoilValue(WorkspaceState)
+  const { handleOpen } = useDialog()
 
-  const router = useRouter();
+  const router = useRouter()
 
   const saveWorkspace = useSWRMutation(
-    "/v1/workspace",
+    '/v1/workspace',
     async (url: string, { arg }: { arg: SaveWorkspaceParam }) =>
       await Post<CommonResponse<void>>(url, arg),
     {
       onSuccess: ({ data }) => {
-        router.push(`/dashboard`);
+        router.push(`/${uuid}/dashboard`)
       },
       onError: (error) => {
         handleOpen({
-          title: "Error",
+          title: 'Error',
           message: error.response.data.message,
-          logLevel: "warn",
-          buttonText: "Close",
-          type: "alert",
-        });
+          logLevel: 'warn',
+          buttonText: 'Close',
+          type: 'alert',
+        })
       },
-    }
-  );
+    },
+  )
 
   const handleImage = (profile: string) => {
-    setValue("profile", profile);
-  };
+    setValue('profile', profile)
+  }
 
   const handleEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleSubmit(onSubmit)();
+    if (e.key === 'Enter') {
+      handleSubmit(onSubmit)()
     }
-  };
+  }
 
   const onSubmit = (data: SaveWorkspaceParam) => {
-    saveWorkspace.trigger(data);
-  };
+    saveWorkspace.trigger(data)
+  }
 
   return (
     <SaveWorkspaceContainer
@@ -89,5 +92,5 @@ export default function Signup(): JSX.Element {
       handleSubmit={handleSubmit}
       onSubmit={onSubmit}
     />
-  );
+  )
 }

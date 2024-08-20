dc1b087520708464d696334fb22e0f0c0c3b884e
pinomaker-hoo <inhoo23@naver.com>
2024-08-20 12:05:27 +0900

    feat(web) : DND 제거

--- apps/web/app/[workspaceUid]/dashboard/task/epic/page.tsx
+++ apps/web/app/[workspaceUid]/dashboard/task/epic/page.tsx
@@ -9,16 +9,11 @@ import { useEffect, useState } from 'react'
 import EpicContainer from '@/src/container/epic-container'
 
 // ** Service Imports
-import useSWR, { mutate } from 'swr'
-import { Get, Patch } from '@/src/repository'
-import useSWRMutation from 'swr/mutation'
+import useSWR from 'swr'
+import { Get } from '@/src/repository'
 
 // ** Type Imports
 import { GetEpicListResponse, SelectContent } from '@/src/type/epic'
-import { CommonResponse } from '@/src/type/common'
-
-// ** Context Imports
-import { useDialog } from '@/src/context/DialogContext'
 
 const EpicConatiner = () => {
   const [word, setWord] = useState<string>('')
@@ -30,32 +25,8 @@ const EpicConatiner = () => {
 
   const searchParams = useSearchParams()
 
-  const { handleOpen } = useDialog()
-
   const { data, error, isLoading } = useSWR('/v1/epic', async (url) =>
     Get<GetEpicListResponse>(url),
-  )
-
-  const updateEpicOrder = useSWRMutation(
-    '/v1/epic/order',
-    async (
-      url: string,
-      { arg }: { arg: { epicId: number; targetEpicId: number } },
-    ) => await Patch<CommonResponse<void>>(url, arg),
-    {
-      onSuccess: ({ data }) => {
-        mutate('/v1/epic')
-      },
-      onError: (error) => {
-        handleOpen({
-          title: 'Error',
-          message: error.response.data.message,
-          logLevel: 'warn',
-          buttonText: 'Close',
-          type: 'alert',
-        })
-      },
-    },
   )
 
   useEffect(() => {
@@ -87,7 +58,6 @@ const EpicConatiner = () => {
       selectContent={selectContent}
       setSelectContent={setSelectContent}
       isLoading={isLoading}
-      updateOrder={updateEpicOrder.trigger}
     />
   )
 }
--- apps/web/src/components/Task/Epic/EpicTable/index.tsx
+++ apps/web/src/components/Task/Epic/EpicTable/index.tsx
@@ -13,60 +13,31 @@ import EpicAddItem from '../EpicAddItem'
 import { EpicInfo, SelectContent } from '@/src/type/epic'
 
 // ** Utils Imports
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import update from 'immutability-helper'
 
 interface PropsType {
   epicData: EpicInfo[]
-  updateOrder: (arg: { epicId: number; targetEpicId: number }) => void
   handleClick: (value: SelectContent) => void
 }
 
-const EpicTable = ({ epicData, handleClick, updateOrder }: PropsType) => {
+const EpicTable = ({ epicData, handleClick }: PropsType) => {
   const { role } = useRecoilValue(WorkspaceState)
-  const [items, setItems] = useState(epicData)
-
-  const moveEpicItem = useCallback(
-    (dragIndex: number, hoverIndex: number) => {
-      const draggedItem = items[dragIndex]
-      const updatedItems = update(items, {
-        $splice: [
-          [dragIndex, 1],
-          [hoverIndex, 0, draggedItem],
-        ],
-      })
-      setItems(updatedItems)
-
-      updateOrder({
-        epicId: draggedItem.epicId,
-        targetEpicId: updatedItems[hoverIndex].epicId,
-      })
-    },
-    [items],
-  )
 
   return (
     <div className="w-full h-full bg-white rounded-[8px] scrollbar-thumb-slate-700 scrollbar-track-slate-300">
       <div className="relative w-full h-full overflow-y-scroll scrollbar-thin">
         <div className="w-full h-full">
-          <DndProvider backend={HTML5Backend}>
-            <table className="w-full text-sm caption-bottom">
-              <tbody className="[&amp;_tr:last-child]:border-0">
-                {items.map((item, index) => (
-                  <EpicItem
-                    key={item.epicId}
-                    index={item.orderId}
-                    item={item}
-                    word=""
-                    moveItem={moveEpicItem}
-                    handleClick={handleClick}
-                  />
-                ))}
-                {role !== 'VIEWER' && <EpicAddItem />}
-              </tbody>
-            </table>
-          </DndProvider>
+          <table className="w-full text-sm caption-bottom">
+            <tbody className="[&amp;_tr:last-child]:border-0">
+              {epicData.map((item, index) => (
+                <EpicItem
+                  key={item.epicId}
+                  item={item}
+                  handleClick={handleClick}
+                />
+              ))}
+              {role !== 'VIEWER' && <EpicAddItem />}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>

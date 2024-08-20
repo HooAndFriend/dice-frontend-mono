6fe1a821ed24aea96b2e9267ec73a94734dc15dd
pinomaker-hoo <inhoo23@naver.com>
2024-08-20 12:07:40 +0900

    fix(web) : build error

--- apps/web/app/[workspaceUid]/dashboard/task/table/page.tsx
+++ apps/web/app/[workspaceUid]/dashboard/task/table/page.tsx
@@ -1,101 +1,74 @@
-"use client";
+'use client'
 
 // ** React Imports
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 // ** Component Imports
-import TableContainer from "@/src/container/table-container";
+import TableContainer from '@/src/container/table-container'
 
 // ** Service Imports
-import { Get, Patch } from "@/src/repository";
-import useSWRMutation from "swr/mutation";
-import useSWR from "swr";
+import { Get } from '@/src/repository'
+import useSWR from 'swr'
 
 // ** Type Imports
-import { GetTicketListResponse } from "@/src/type/ticket";
-import { CommonResponse } from "@/src/type/common";
+import { GetTicketListResponse } from '@/src/type/ticket'
 
 // ** Context Imports
-import { useDialog } from "@/src/context/DialogContext";
-import { WorkspaceUser } from "@/src/type/workspace";
-import { EpicStatus } from "@/src/type/epic";
+import { WorkspaceUser } from '@/src/type/workspace'
+import { EpicStatus } from '@/src/type/epic'
 
 const TablePage = () => {
-  const [word, setWord] = useState<string>("");
-  const [selectedEpicIds, setSelectedEpicIds] = useState<number[]>([]);
-  const [selectedStatus, setSelectedStatus] = useState<EpicStatus[]>([]);
-  const [selectedTypeIds, setSelectedTypeIds] = useState<number[]>([]);
+  const [word, setWord] = useState<string>('')
+  const [selectedEpicIds, setSelectedEpicIds] = useState<number[]>([])
+  const [selectedStatus, setSelectedStatus] = useState<EpicStatus[]>([])
+  const [selectedTypeIds, setSelectedTypeIds] = useState<number[]>([])
 
-  const [ticketId, setTicketId] = useState<number>(0);
-  const [checkedList, setCheckedList] = useState<WorkspaceUser[]>([]);
+  const [ticketId, setTicketId] = useState<number>(0)
+  const [checkedList, setCheckedList] = useState<WorkspaceUser[]>([])
 
-  const [enabled, setEnabled] = useState<boolean>(false);
-
-  const { handleOpen } = useDialog();
+  const [enabled, setEnabled] = useState<boolean>(false)
 
   const {
     data,
     error,
     isLoading,
     mutate: handleTicketRefetch,
-  } = useSWR("/v1/ticket", async (url) => Get<GetTicketListResponse>(url));
-
-  const updateOrder = useSWRMutation(
-    "/v1/ticket/order",
-    async (
-      url: string,
-      { arg }: { arg: { ticketId: number; targetTicketId: number } }
-    ) => await Patch<CommonResponse<void>>(url, arg),
-    {
-      onSuccess: ({ data }) => {
-        handleTicketRefetch();
-      },
-      onError: (error) => {
-        handleOpen({
-          title: "Error",
-          message: error.response.data.message,
-          logLevel: "warn",
-          buttonText: "Close",
-          type: "alert",
-        });
-      },
-    }
-  );
+  } = useSWR('/v1/ticket', async (url) => Get<GetTicketListResponse>(url))
 
   const handleEpicSelectFilter = (epicId: number) => {
     if (selectedEpicIds.includes(epicId)) {
-      setSelectedEpicIds((c) => c.filter((id) => id !== epicId));
+      setSelectedEpicIds((c) => c.filter((id) => id !== epicId))
     } else {
-      setSelectedEpicIds((c) => [...c, epicId]);
+      setSelectedEpicIds((c) => [...c, epicId])
     }
-  };
+  }
 
   const handleStatusSelectFilter = (status: EpicStatus) => {
     if (selectedStatus.includes(status)) {
-      setSelectedStatus((c) => c.filter((s) => s !== status));
+      setSelectedStatus((c) => c.filter((s) => s !== status))
     } else {
-      setSelectedStatus((c) => [...c, status]);
+      setSelectedStatus((c) => [...c, status])
     }
-  };
+  }
 
   const handleTypeSelectFilter = (typeId: number) => {
     if (selectedTypeIds.includes(typeId)) {
-      setSelectedTypeIds((c) => c.filter((id) => id !== typeId));
+      setSelectedTypeIds((c) => c.filter((id) => id !== typeId))
     } else {
-      setSelectedTypeIds((c) => [...c, typeId]);
+      setSelectedTypeIds((c) => [...c, typeId])
     }
-  };
+  }
 
   useEffect(() => {
-    const animation = requestAnimationFrame(() => setEnabled(true));
+    const animation = requestAnimationFrame(() => setEnabled(true))
 
     return () => {
-      cancelAnimationFrame(animation);
-      setEnabled(false);
-    };
-  }, []);
+      cancelAnimationFrame(animation)
+      setEnabled(false)
+    }
+  }, [])
 
-  if (error || !enabled) return;
+  if (error || !enabled) return
 
   return (
     <TableContainer
@@ -110,13 +83,12 @@ const TablePage = () => {
       handleTypeSelectFilter={handleTypeSelectFilter}
       handleStatusSelectFilter={handleStatusSelectFilter}
       handleEpicSelectFilter={handleEpicSelectFilter}
-      updateOrder={updateOrder.trigger}
       handleWord={(e) => setWord(e.target.value)}
       setCheckedList={setCheckedList}
       setTicketId={setTicketId}
       isLoading={isLoading}
     />
-  );
-};
+  )
+}
 
-export default TablePage;
+export default TablePage

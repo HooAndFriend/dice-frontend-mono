b75a4c89728020d36bd0aa2e6186729008720b5e
pinomaker-hoo <inhoo23@naver.com>
2024-09-03 10:58:40 +0900

    feat(web) : HAF-72(due date 삭제)

--- apps/web/src/components/Task/Ticket/TicketDatePicker/index.tsx
+++ apps/web/src/components/Task/Ticket/TicketDatePicker/index.tsx
@@ -1,18 +1,13 @@
-// ** Service Imports
 import { mutate } from 'swr'
 import { Patch } from '@/src/repository'
 import useSWRMutation from 'swr/mutation'
-
-// ** Type Imports
 import { CommonResponse } from '@/src/type/common'
-
-// ** Context Imports
 import { useDialog } from '@/src/context/DialogContext'
 import { useState } from 'react'
 import CustomImage from '../../../Image/CustomImage'
 
 interface PropsType {
-  value: string
+  value: string | null // 날짜가 null일 수 있도록 타입 수정
   ticketId: number
 }
 
@@ -24,7 +19,7 @@ const TicketDatePicker = ({ value, ticketId }: PropsType) => {
 
   const updateTicket = useSWRMutation(
     'v1/ticket/dueDate',
-    async (url: string, { arg }: { arg: string }) =>
+    async (url: string, { arg }: { arg: string | null }) =>
       await Patch<CommonResponse<void>>(url, { ticketId, dueDate: arg }),
     {
       onSuccess: () => {
@@ -46,18 +41,27 @@ const TicketDatePicker = ({ value, ticketId }: PropsType) => {
   )
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    updateTicket.trigger(e.target.value)
+    updateTicket.trigger(e.target.value || null)
+  }
+
+  const handleClearDate = () => {
+    updateTicket.trigger(null)
   }
 
   return (
     <div>
       {open ? (
-        <input
-          type="date"
-          value={value}
-          onChange={handleOnChange}
-          className="h-[40px] w-[240px] border-none bg-none"
-        />
+        <div className="flex items-center">
+          <input
+            type="date"
+            value={value || ''}
+            onChange={handleOnChange}
+            className="h-[40px] w-[200px] border-none bg-none"
+          />
+          <button onClick={handleClearDate} className="ml-[8px] text-red-500">
+            Clear
+          </button>
+        </div>
       ) : (
         <div
           className="flex items-center px-[16px] h-[40px] bg-[#F2F4F6] rounded-[5px] w-[240px] justify-between cursor-pointer"

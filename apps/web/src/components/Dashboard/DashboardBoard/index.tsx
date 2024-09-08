951dc669187dd166f995ff0baa06884c19a6af60
pinomaker-hoo <inhoo23@naver.com>
2024-09-09 00:52:46 +0900

    feat(web) : HAF-109(게시글 콘텐츠 없으면 No Data UI 추가)

--- apps/web/src/components/Dashboard/DashboardBoard/index.tsx
+++ apps/web/src/components/Dashboard/DashboardBoard/index.tsx
@@ -4,6 +4,7 @@ import { Get } from '@/src/repository'
 import { GetBoardListResponse } from '@/src/type/board'
 import useSWR from 'swr'
 import DashboardBoardItem from './components/DashboardBoardItem'
+import Image from 'next/image'
 
 interface PropsType {}
 
@@ -19,10 +20,20 @@ const DashboardBoard = ({}: PropsType) => {
     <div className="bg-white rounded-[20px] shadow-md p-[24px] lg:col-span-1 flex-1">
       <h1 className="text-2xl font-bold">Recent wiki</h1>
       <div className="grid gap-4 pt-[16px]">
-        {!isLoading &&
+        {!isLoading && data.data.data.length > 0 ? (
           data.data.data.map((item) => (
             <DashboardBoardItem data={item} key={item.boardId} />
-          ))}
+          ))
+        ) : (
+          <div className="flex items-center justify-center h-full text-muted-foreground">
+            <Image
+              src="/images/no_board.jpeg"
+              width={200}
+              height={200}
+              alt="No Data UI"
+            />
+          </div>
+        )}
       </div>
     </div>
   )

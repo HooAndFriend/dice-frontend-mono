8350e0d88110ab60c452e1bf622ce52c1ae962c0
pinomaker-hoo <inhoo23@naver.com>
2025-01-30 16:28:14 +0900

    feat(web) : HAF-52

--- apps/web/src/components/Tooltip/index.tsx
+++ apps/web/src/components/Tooltip/index.tsx
@@ -16,7 +16,12 @@ const Tooltip = ({ children, text, width = 32, isFull = false }) => {
       </div>
       {isHovered && text && (
         <div
-          className={`absolute z-10 w-${width} p-2 text-xs text-white transition-opacity duration-300 transform -translate-x-1/2 bg-gray-800 rounded-[6px] shadow-lg opacity-100 pointer-events-none bottom-full left-1/2`}
+          className="absolute z-10 p-2 text-xs text-white transition-opacity duration-300 transform -translate-x-1/2 bg-gray-800 rounded-[6px] shadow-lg opacity-100 pointer-events-none bottom-full left-1/2"
+          style={{
+            width: isFull ? `${width}px` : 'auto',
+            minWidth: 'max-content',
+            whiteSpace: 'nowrap',
+          }}
         >
           {text}
         </div>
@@ -24,4 +29,5 @@ const Tooltip = ({ children, text, width = 32, isFull = false }) => {
     </div>
   )
 }
+
 export default Tooltip
